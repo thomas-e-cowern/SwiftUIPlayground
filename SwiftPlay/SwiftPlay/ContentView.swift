8b25a53d050eef2c4b0b447d0f627f03a0bff2a9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-22 04:31:10 -0500

    initial commit

---
+++ SwiftPlay/SwiftPlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPlay
+//
+//  Created by Thomas Cowern New on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPlay/SwiftPlay/SwiftPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlayApp.swift
+//  SwiftPlay
+//
+//  Created by Thomas Cowern New on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
