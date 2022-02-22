9b9d1e6c5aeee5eeaf67fe1de32009961077ec80
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-22 04:49:32 -0500

    added some stacks

--- SwiftPlay/SwiftPlay/ContentView.swift
+++ SwiftPlay/SwiftPlay/ContentView.swift
@@ -9,8 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack (spacing: 5) {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 100, height: 100)
+                    
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+            } // End of VStack
+            .border(.pink, width: 12)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+            
+            VStack (spacing: 5) {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 100, height: 100)
+                    
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+            } // End of VStack
+            .border(.yellow, width: 12)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ZStack () {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 100, height: 100)
+                    .offset(y: -100)
+                    
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+                    .offset(y: 100)
+            }
+        } // End of HStack
     }
 }
 
