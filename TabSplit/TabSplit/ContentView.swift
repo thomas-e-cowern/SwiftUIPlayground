3ba192fa6c8962bdf0457c9301367c010d572cae
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-01 09:07:53 -0500

    added binding, picker and buttons becuase its fun

--- TabSplit/TabSplit/ContentView.swift
+++ TabSplit/TabSplit/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State var tapCount = 0
+//    @State private var name: String = ""
+
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
         NavigationView {
+//            Form {
+//                Text("Hello, world!")
+//
+//                Button {
+//                    self.tapCount += 1
+//                } label: {
+//                    Text("Tap Count \(tapCount)")
+//                }
+//
+//            }
+//            .navigationTitle("Tab Split")
+//            .navigationBarTitleDisplayMode(.inline)
+        
             Form {
-                Text("Hello, world!")
+//                TextField("Enter your name", text: $name)
+//                Text("Hello, \(name)")
+                
+//                ForEach(0..<100) {
+//                    Text("Row \($0)")
+//                }
+                
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
             }
             .navigationTitle("Tab Split")
             .navigationBarTitleDisplayMode(.inline)
