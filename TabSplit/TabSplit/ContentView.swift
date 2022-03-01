69ce8d41f406df51cf398df75f91bb72eed87774
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-01 10:54:44 -0500

    added propeties and basic form to content view

--- TabSplit/TabSplit/ContentView.swift
+++ TabSplit/TabSplit/ContentView.swift
@@ -9,44 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State var tapCount = 0
-//    @State private var name: String = ""
-
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount: Float = 0.0
+    @State private var numberOfPeople: Int = 0
+    @State private var tipPercentage: Float = 15.0
+    
+    let tipPercentages = [0.0, 5.0, 10.0, 15.0, 20.0, 25.0]
     
     var body: some View {
-        NavigationView {
-//            Form {
-//                Text("Hello, world!")
-//
-//                Button {
-//                    self.tapCount += 1
-//                } label: {
-//                    Text("Tap Count \(tapCount)")
-//                }
-//
-//            }
-//            .navigationTitle("Tab Split")
-//            .navigationBarTitleDisplayMode(.inline)
-        
-            Form {
-//                TextField("Enter your name", text: $name)
-//                Text("Hello, \(name)")
-                
-//                ForEach(0..<100) {
-//                    Text("Row \($0)")
-//                }
-                
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-                
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
             }
-            .navigationTitle("Tab Split")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
