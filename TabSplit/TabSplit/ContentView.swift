55b6e90e7609986a9c615042401763a12f9a87d0
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-01 11:11:40 -0500

    added picker view to content view

--- TabSplit/TabSplit/ContentView.swift
+++ TabSplit/TabSplit/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount: Float = 0.0
-    @State private var numberOfPeople: Int = 0
+    @State private var numberOfPeople: Int = 2
     @State private var tipPercentage: Float = 15.0
     
     let tipPercentages = [0.0, 5.0, 10.0, 15.0, 20.0, 25.0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                        .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                            .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(1..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                }
             }
+            .navigationTitle("Tab Split")
         }
     }
 }
