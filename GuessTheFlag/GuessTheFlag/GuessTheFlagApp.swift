e75173c29695288c7eeaabd3a318e3b3f64be208
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-03 07:09:56 -0500

    guess the flag initial commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Thomas Cowern New on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
