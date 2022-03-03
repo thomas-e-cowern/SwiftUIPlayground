e1cb3c48f7cc5c7db7873e60a7bbc8ef1e0b7b45
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-03 08:12:46 -0500

    added initial flag and text view to content view

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -13,9 +13,20 @@ struct ContentView: View {
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack {
-            Text("Tap the flag of ")
-            Text(countries[correctAnswer])
+        VStack (spacing: 30) {
+            VStack {
+                Text("Tap the flag of ")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                    // flag
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+            }
         }
     }
 }
