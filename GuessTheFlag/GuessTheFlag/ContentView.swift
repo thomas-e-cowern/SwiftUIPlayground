1821fdfe6ff85204f7cb90f6b8a33f4c41347dfb
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-03 08:25:03 -0500

    added flag tapped function to content view

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,25 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "treland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var showingScore: Bool = false
+    @State private var scoreTitle: String = ""
+    
+    var countries = ["Estonia", "France", "Germany", "treland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack (spacing: 30) {
-            VStack {
-                Text("Tap the flag of ")
-                Text(countries[correctAnswer])
-            }
+        ZStack {
             
-            ForEach(0..<3) { number in
-                Button {
-                    // flag
-                } label: {
-                    Image(countries[number])
-                        .renderingMode(.original)
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack (spacing: 30) {
+                VStack {
+                    Text("Tap the flag of ")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
                 }
             }
         }
+    }
+    
+    func flagTapped (_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Incorrect"
+        }
+        showingScore = true
     }
 }
 
