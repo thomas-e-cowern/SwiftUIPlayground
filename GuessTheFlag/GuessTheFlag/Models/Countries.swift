895cd567b855b38b19e32a80ec104b49557e5c16
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-04 09:38:46 -0500

    added countries model

---
+++ GuessTheFlag/GuessTheFlag/Models/Countries.swift
@@ -0,0 +1,13 @@
+//
+//  Countries.swift
+//  GuessTheFlag
+//
+//  Created by Thomas Cowern New on 3/4/22.
+//
+
+import Foundation
+
+struct Country: Codable, Hashable {
+    var code: String
+    var name: String
+}
---
+++ GuessTheFlag/GuessTheFlag/Views/FlagAPIView.swift
@@ -0,0 +1,21 @@
+//
+//  FlagAPIView.swift
+//  GuessTheFlag
+//
+//  Created by Thomas Cowern New on 3/4/22.
+//
+
+import SwiftUI
+
+struct FlagAPIView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlagAPIView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagAPIView()
+    }
+}
