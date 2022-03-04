d1915b4e85ff2d81552c59e77b7a7e336ace8010
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-04 10:52:22 -0500

    views and modifiers initial commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Thomas Cowern New on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
