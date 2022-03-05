652683d09b033732f752e4d1278f2c901ffede5d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-05 09:19:05 -0500

    word scramble initial commit

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Hello, world!")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.pink.opacity(0.5))
+            
+            Color.blue
+                .frame(width: 100, height: 100)
+                .watermarked(with: "Hacking with Tom")
+            
+            Button("Click Me"){
+                print(type(of: self.body))
+            }
+            .foregroundColor(.white)
             .padding()
+            .background(.blue)
+            .padding()
+            .background(.red)
+            .padding()
+            .background(.yellow)
+            .padding()
+            .background(.orange)
+        }
     }
 }
 
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Thomas Cowern New on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
