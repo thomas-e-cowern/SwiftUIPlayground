bdb633914bbf0c4b7423c81c582f11d9b20d652f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-05 14:58:09 -0500

    animations playground initial commit

---
+++ AnimationsPlayground/AnimationsPlayground/AnimationsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsPlaygroundApp.swift
+//  AnimationsPlayground
+//
+//  Created by Thomas Cowern New on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
