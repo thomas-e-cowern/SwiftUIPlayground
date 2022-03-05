b72a4d0cfc857fb2bf965b45416699a14689aa80
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-05 09:27:08 -0500

    added properties to content view

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK:  Properties
+    @State private var usedWords: [String] = []
+    @State private var rootWord: String = ""
+    @State private var newWord: String = ""
+    
+    // MARK:  Body
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(0..<5) {
+            Text("Dynamic row \($0)")
+        }
     }
 }
 
+// MARK:  Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
