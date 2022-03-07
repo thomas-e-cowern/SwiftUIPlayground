c2447963bc26181767449c79a583984f19822580
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-07 11:34:09 -0500

    added expense item struct

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Thomas Cowern New on 3/7/22.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
