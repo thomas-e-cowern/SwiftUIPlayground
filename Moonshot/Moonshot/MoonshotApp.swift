c8e9b58ab3ce447789a2ab8283f224b20b46ad83
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-08 14:41:43 -0500

    moonshot initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Thomas Cowern New on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
