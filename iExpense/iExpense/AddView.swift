9a3f19f6fb1e48a9898883c387a6baba336fee16
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-08 07:34:26 -0500

    added add view

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Thomas Cowern New on 3/8/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
