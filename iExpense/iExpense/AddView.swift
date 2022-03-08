cf2787f81fb497f125a1b5548c278c400a3278ae
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-08 07:42:08 -0500

    added form, properties and controls to add view

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Bussiness", "Personal"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+        }
     }
 }
 
