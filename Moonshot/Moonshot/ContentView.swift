b537983e9d89d8e79c61e0077fef35333c7319c3
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-09 04:24:37 -0500

    added computed properties to missions

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -12,9 +12,37 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Text("Astronuats: \(astronauts.count)")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
         }
     }
 }
--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -9,13 +9,25 @@ import Foundation
 
 struct Mission: Codable, Identifiable {
     
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apallo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
     struct CrewRole: Codable {
         let name: String
         let role: String
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
 }
