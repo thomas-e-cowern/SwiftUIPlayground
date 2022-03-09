ab655eb1bee153d9b11727b3d55ab82fb76886b3
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-09 09:30:00 -0500

    added layout, struct to mission view

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct MissionView: View {
     
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
     
     var body: some View {
         GeometryReader { geometry in
@@ -29,6 +35,27 @@ struct MissionView: View {
                            Text(mission.description)
                        }
                        .padding(.horizontal)
+                       
+                       ScrollView(.horizontal, showsIndicators: false) {
+                           HStack {
+                               ForEach(crew, id: \.role) { crewMember in
+                                   NavigationLink {
+                                       Text("Astronaut Details")
+                                   } label : {
+                                       HStack {
+                                           Image(crewMember.astronaut.id)
+                                               .resizable()
+                                               .frame(width: 104, height: 72)
+                                               .clipShape(Capsule())
+                                               .overlay(
+                                                    Capsule()
+                                                        .strokeBorder(.white, lineWidth: 1)
+                                               )
+                                       }
+                                   }
+                               }
+                           }
+                       }
                    }
                    .padding(.bottom)
                }
@@ -37,14 +64,27 @@ struct MissionView: View {
            .navigationBarTitleDisplayMode(.inline)
            .background(.darkBackground)
     }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
     
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("atrounauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
                     .preferredColorScheme(.dark)
     }
 }
