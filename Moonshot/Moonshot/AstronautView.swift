ee063a3bfaeba49efda615186d76072f4d9518f3
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-10 14:47:33 -0500

    added astronaut view

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,20 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Thomas Cowern New on 3/10/22.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static var previews: some View {
+        AstronautView()
+    }
+}
