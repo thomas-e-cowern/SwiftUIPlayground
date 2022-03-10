0b8e3ef57a94dc0944c215b9dbee6895229cd770
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-10 14:56:04 -0500

    added scroll view and field to astronaut view

--- Moonshot/Moonshot/AstronautView.swift
+++ Moonshot/Moonshot/AstronautView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct AstronautView: View {
+    
+    let astronaut: Astronaut
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack {
+                
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct AstronautView_Previews: PreviewProvider {
+    
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
     static var previews: some View {
-        AstronautView()
+        AstronautView(astronaut: astronauts["aldrin"]!)
     }
 }
