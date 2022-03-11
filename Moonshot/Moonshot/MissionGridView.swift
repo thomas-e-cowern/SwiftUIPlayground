fd62882aef3649ae1a8ac5c3d0734c2be0d054ce
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-11 13:39:28 -0500

    added view buton to  content view

--- Moonshot/Moonshot/MissionGridView.swift
+++ Moonshot/Moonshot/MissionGridView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MissionGridView: View {
     
-    @State private var showGrid: Bool = true
+    
     
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
@@ -19,54 +19,45 @@ struct MissionGridView: View {
     ]
     
     var body: some View {
-        
-        Group {
-            if showGrid {
-                MissionGridView()
-            } else {
-                MissionListView()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
             }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
-        
-//        NavigationView {
-//            ScrollView {
-//                LazyVGrid(columns: columns) {
-//                    ForEach(missions) { mission in
-//                        NavigationLink {
-//                            MissionView(mission: mission, astronauts: astronauts)
-//                        } label: {
-//                            VStack {
-//                                Image(mission.image)
-//                                    .resizable()
-//                                    .scaledToFit()
-//                                    .frame(width: 100, height: 100)
-//                                
-//                                VStack {
-//                                    Text(mission.displayName)
-//                                        .font(.headline)
-//                                        .foregroundColor(.white)
-//                                    Text(mission.formattedLaunchDate)
-//                                        .font(.caption)
-//                                        .foregroundColor(.white.opacity(0.5))
-//                                }
-//                                .padding(.vertical)
-//                                .frame(maxWidth: .infinity)
-//                                .background(.lightBackground)
-//                            }
-//                            .clipShape(RoundedRectangle(cornerRadius: 10))
-//                            .overlay(
-//                                RoundedRectangle(cornerRadius: 10)
-//                                    .stroke(.lightBackground)
-//                            )
-//                        }
-//                    }
-//                }
-//                .padding([.horizontal, .bottom])
-//            }
-//            .navigationTitle("Moonshot")
-//            .background(.darkBackground)
-//            .preferredColorScheme(.dark)
-//        }
     }
 }
 
