0e6d2442283892c9d9a6166bfeefa5d885a3412e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-14 08:51:16 -0400

    added nav view, form, and picker to content view

--- Cupcakes/Cupcakes/ContentView.swift
+++ Cupcakes/Cupcakes/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var order = Order()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cupcake flavor", selection: $order.type) {
+                        ForEach(Order.type.indices) {
+                            Text(Order.type[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cupcakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+            }
+            .navigationBarTitle("Cupcakes")
+        }
     }
 }
 
