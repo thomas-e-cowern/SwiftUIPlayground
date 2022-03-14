d682ef16ae02d57e1f28c263397cf98479ca33cb
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-14 10:47:23 -0400

    added computed cost to order model and updated checkout view

--- Cupcakes/Cupcakes/CheckoutView.swift
+++ Cupcakes/Cupcakes/CheckoutView.swift
@@ -12,7 +12,30 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text("Hello Checkout")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                 
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order") {
+                    // Order would be placed
+                }
+                .padding()
+                    
+            }
+            .navigationTitle("Check Out")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
--- Cupcakes/Cupcakes/Order.swift
+++ Cupcakes/Cupcakes/Order.swift
@@ -14,6 +14,14 @@ class Order: ObservableObject {
     @Published var type: Int = 0
     @Published var quantity: Int = 3
     
+    @Published var extraFrosting: Bool = false
+    @Published var addSprinkles: Bool = false
+    
+    @Published var name: String = ""
+    @Published var street: String = ""
+    @Published var city: String = ""
+    @Published var zip: String = ""
+    
     @Published var specialRequestEnabled: Bool = false {
         didSet {
             if specialRequestEnabled == false {
@@ -31,11 +39,23 @@ class Order: ObservableObject {
         return true
     }
     
-    @Published var extraFrosting: Bool = false
-    @Published var addSprinkles: Bool = false
-    
-    @Published var name: String = ""
-    @Published var street: String = ""
-    @Published var city: String = ""
-    @Published var zip: String = ""
+    var cost: Double {
+        // $2 per cupcake
+        var cost = Double(quantity) * 2
+        
+        // complicate cupcakes are more
+        cost += (Double(type)/2)
+        
+        // $1 for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        // .50 for extra sprinkles
+        if addSprinkles {
+            cost += Double(quantity)/2
+        }
+        
+        return cost
+    }
 }
