478bd4e427c6ddec154659e1f97f92631d8966fc
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-14 08:13:32 -0400

    cupcakes initial commit

---
+++ Cupcakes/Cupcakes/CupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesApp.swift
+//  Cupcakes
+//
+//  Created by Thomas Cowern New on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
