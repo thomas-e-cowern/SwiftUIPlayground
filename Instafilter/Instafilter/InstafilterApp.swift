3a5be0305d888f579c95cc99ccf56133ec49968d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-17 14:16:16 -0400

    instafilter initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Thomas Cowern New on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
