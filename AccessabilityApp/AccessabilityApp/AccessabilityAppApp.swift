d12151f1ba1d341cfedaaeea23792283c5812bfa
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-22 06:34:17 -0400

    accessability app iniital commit

---
+++ AccessabilityApp/AccessabilityApp/AccessabilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessabilityAppApp.swift
+//  AccessabilityApp
+//
+//  Created by Thomas Cowern New on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessabilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
