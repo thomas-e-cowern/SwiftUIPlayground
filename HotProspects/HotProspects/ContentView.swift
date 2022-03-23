31b193024fa95773d87407699afeb565635c5a1e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-23 15:02:32 -0400

    added custom swipe actions to content view

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -45,9 +45,8 @@ struct DisplayView: View {
 
 struct ContentView: View {
     
-    @State private var output = ""
-    
-    @State private var backgroundColor = Color.red
+//    @State private var output = ""
+//    @State private var backgroundColor = Color.red
 //    @StateObject private var updater = DelayedUpdater()
 //    static let tag = "ContentView"
 //    @StateObject var user = User()
@@ -55,89 +54,25 @@ struct ContentView: View {
     
     var body: some View {
         
-//        Image("example")
-//            .interpolation(.none)
-//            .resizable()
-//            .scaledToFit()
-//            .frame(maxHeight: .infinity)
-//            .background(.black)
-//            .ignoresSafeArea()
-      VStack {
-            Text("Hello World!")
-              .padding()
-              .background(backgroundColor)
-          
-          Text("Change Color")
-              .padding()
-              .contextMenu {
-                  Button {
-                      backgroundColor = .red
-                  } label: {
-                      Label("Red", systemImage: "checkmark.circle.fill")
-                  }
-                  
-                  Button {
-                      backgroundColor = .green
-                  } label: {
-                      Label("Green", systemImage: "checkmark.circle.fill")
-                  }
-                  
-                  Button {
-                      backgroundColor = .blue
-                  } label: {
-                      Label("Blue", systemImage: "checkmark.circle.fill")
-                  }
-              }
-      }
-//            .task {
-//                await fetchReadings()
-//            }
         
-//        VStack {
-//            EditView()
-//            DisplayView()
-//        }
-//        .environmentObject(user)
-    
-//        TabView (selection: $selectedTab) {
-//            Text("Tab 1")
-//                .onTapGesture {
-//                    selectedTab = "Two"
-//                }
-//                .tabItem {
-//                    Label("One", systemImage: "star")
-//                }
-//            Text("Tab 2")
-//                .tabItem {
-//                    Label("Two", systemImage: "circle")
-//                }
-//                .tag("Two")
-//        }
-        
-    }
-    
-    func fetchReadings () async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
+        List {
+            Text("Taylor Swift")
+                .swipeActions {
+                    Button (role: .destructive) {
+                        print("Holy")
+                    } label: {
+                        Label("Delete", systemImage: "minus.circle")
+                    }
+                }
+                .swipeActions (edge: .leading) {
+                    Button {
+                        print("Smokes")
+                    } label: {
+                        Label("Pin", systemImage: "pin")
+                    }
+                    .tint(.orange)
+                }
         }
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download error: \(error)"
-        }
-        
-//        do {
-//            output = try result.get()
-//        } catch {
-//            print("Download error")
-//        }
     }
 }
 
