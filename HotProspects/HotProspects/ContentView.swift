72d91d6aedd94d2a557dcc27a33099321cca59a0
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-24 11:14:01 -0400

    added prospect and my view and tab view to content view

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,52 +6,29 @@
 //
 
 import SwiftUI
-import UserNotifications
-import SamplePackage
 
 struct ContentView: View {
-    
-    let possibleNumbers = Array(1...60)
-    
     var body: some View {
         
-        Text(results)
-            .padding()
+        TabView {
+            ProspectView()
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectView()
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectView()
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MyView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
         
-        
-//        VStack {
-//            Button("Request permission") {
-//                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-//                    if success {
-//                        print("All Set")
-//                    } else if let error = error {
-//                        print("Error: \(error.localizedDescription)")
-//                    }
-//                }
-//            }
-//
-//            Button("Schedule notification") {
-//                let content = UNMutableNotificationContent()
-//                content.title = "Feed the cat"
-//                content.subtitle = "He looks hungry"
-//                content.sound = UNNotificationSound.default
-//
-//                // show it 5 seconds from now
-//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-//
-//                // choose a random identifier
-//                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-//
-//                // add the notification request
-//                UNUserNotificationCenter.current().add(request)
-//            }
-//        }
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
     }
 }
 
---
+++ HotProspects/HotProspects/MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  HotProspects
+//
+//  Created by Thomas Cowern New on 3/24/22.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyView()
+    }
+}
