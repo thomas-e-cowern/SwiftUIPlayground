7e60f66ede4c525e3340341cba70f446b20bf923
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-03-25 09:57:08 -0400

    added qrcode generator to my view

--- HotProspects/HotProspects/MyView.swift
+++ HotProspects/HotProspects/MyView.swift
@@ -6,10 +6,50 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MyView: View {
+    
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+            }
+            .navigationTitle("Your code")
+        }
+        
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
