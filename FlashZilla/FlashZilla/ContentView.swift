dd2eeea872d989007e796a6205f16c772ccc719f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-05 15:19:00 -0400

    added zoom gesture to content view

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentAmount = 0.0
+    @State private var finalAmout = 1.0
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .scaleEffect(finalAmout + currentAmount)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { amount in
+                        currentAmount = amount - 1
+                    }
+                    .onEnded { amount in
+                        finalAmout += currentAmount
+                        currentAmount = 0
+                    }
+            )
     }
 }
 
