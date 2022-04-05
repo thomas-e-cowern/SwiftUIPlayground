82d4aeb6b6d548e0a4ddc1ca31b744f8e21e82dd
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-05 16:15:16 -0400

    added rotate gesture to content view

--- FlashZilla/FlashZilla/ContentView.swift
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -9,23 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var currentAmount = 0.0
-    @State private var finalAmout = 1.0
+//    @State private var currentAmount = 0.0
+//    @State private var finalAmount = 1.0
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
+
     
     var body: some View {
         Text("Hello, world!")
             .padding()
-            .scaleEffect(finalAmout + currentAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { amount in
-                        currentAmount = amount - 1
-                    }
-                    .onEnded { amount in
-                        finalAmout += currentAmount
-                        currentAmount = 0
-                    }
-            )
+            .rotationEffect(currentAmount + finalAmount)
+                .gesture(
+                    RotationGesture()
+                        .onChanged { angle in
+                            currentAmount = angle
+                        }
+                        .onEnded { angle in
+                            finalAmount += angle
+                            currentAmount = .zero
+                        }
+                )
+//            .scaleEffect(finalAmount + currentAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged { amount in
+//                        currentAmount = amount - 1
+//                    }
+//                    .onEnded { amount in
+//                        finalAmount += currentAmount
+//                        currentAmount = 0
+//                    }
+//            )
+//            .onTapGesture (count: 3) {
+//                finalAmount = 1.0
+//            }
     }
 }
 
