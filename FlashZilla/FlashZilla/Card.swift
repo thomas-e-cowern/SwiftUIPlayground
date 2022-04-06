4fd61f7fa815d9bf07d4726117ae6f47b2b34b83
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-06 13:17:21 -0400

    added card model

---
+++ FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Thomas Cowern New on 4/6/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "What day is today?", answer: "Today!")
+}
