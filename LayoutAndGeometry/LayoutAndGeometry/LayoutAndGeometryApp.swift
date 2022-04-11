caadf48dd31aa25840d7525324e624ce2d052e4a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-11 07:44:14 -0400

    layout and geometry initial commit

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Thomas Cowern New on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
