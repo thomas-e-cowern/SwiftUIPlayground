ea2458043993b8240e9df50e2158a73a19846e35
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-12 09:18:54 -0400

    added inner view and outer view to content view

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -17,13 +17,45 @@ import SwiftUI
 //    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 //}
 
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            
+            GeometryReader { geo in
+                
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global Center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midX)")
+                        print("Local Center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midX)")
+                        print("Custom Center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midX)")
+                    }
+                
+            }
+            .background(.orange)
+            
+            Text("Right")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Text("Hello World!")
-                .frame(width: geo.size.width * 0.9, height: geo.size.height * 0.9, alignment: .center)
-                .background(.red)
-        }
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
