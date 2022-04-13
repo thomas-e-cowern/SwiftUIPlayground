b64e4367dd430a606260063e7c97175264d4a788
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-13 09:40:13 -0400

    added horizontal size class environemnt propertyto content view

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,37 +7,54 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Thomas Cowern")
+            Text("Country: USA")
+            Text("Pets: Lily 😥")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
     
-    @State private var selectedUser: User? = nil
-    @State private var isShowingAlert = false
+    @Environment(\.horizontalSizeClass) var sizeClass
+//    @State private var selectedUser: User? = nil
+    @State private var layoutVertically = true
     
     var body: some View {
-        Text("Hello World")
-            .padding()
-            .onTapGesture {
-                selectedUser = User()
-            }
-            .sheet(item: $selectedUser) { user in
-                Text(user.id)
-            }
-        Text("Alert Me")
-            .onTapGesture {
-                selectedUser = User()
-                isShowingAlert.toggle()
-            }
-            .alert("Welcome", isPresented: $isShowingAlert, presenting: selectedUser) { user in
-                Button(user.id) {
-                    
+
+        if sizeClass == .compact {
+            VStack(content: UserView.init)
+            } else {
+                HStack {
+                    UserView()
                 }
             }
+
         
         
         
+        
+//            .padding()
+//            .onTapGesture {
+//                selectedUser = User()
+//            }
+//            .sheet(item: $selectedUser) { user in
+//                Text(user.id)
+//            }
+//        Text("Alert Me")
+//            .onTapGesture {
+//                selectedUser = User()
+//                isShowingAlert.toggle()
+//            }
+//            .alert("Welcome", isPresented: $isShowingAlert, presenting: selectedUser) { user in
+//                Button(user.id) {
+//
+//                }
+//            }
         
 //        NavigationView {
 //            NavigationLink {
