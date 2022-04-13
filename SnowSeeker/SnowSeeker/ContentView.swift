1a0e950a793ab4d68e0407984a0b8be5e8a424f0
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-13 09:05:37 -0400

    added struct user to content view

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,20 +7,32 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    
+    
+    
     var body: some View {
-        NavigationView {
-            NavigationLink {
-                Text("New Secondary")
-            } label: {
-                Text("Hello World")
-            }
-            .navigationTitle("Primary")
-            
-            Text("Secondary")
-            
-            Text("Tertiary")
-        }
+        Text("Hello World")
+        
+        
+        
+        
+//        NavigationView {
+//            NavigationLink {
+//                Text("New Secondary")
+//            } label: {
+//                Text("Hello World")
+//            }
+//            .navigationTitle("Primary")
+//
+//            Text("Secondary")
+//
+//            Text("Tertiary")
+//        }
     }
 }
 
