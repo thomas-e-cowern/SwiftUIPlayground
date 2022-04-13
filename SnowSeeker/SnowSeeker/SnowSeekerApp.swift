1b4c699df6e6bba261cf29d7fdb38bf973bd4009
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-13 08:01:31 -0400

    snow seeker initial commit

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Thomas Cowern New on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
