c7f49b4e036a9da5859dfc66cf9b92043ea0eb49
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-13 11:51:23 -0400

    added navigation view, links, and list to content view

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -15,8 +15,25 @@ struct ContentView: View {
         
         NavigationView {
             List(resorts) { resort in
-                Text(resort.name)
-            }
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                            .stroke(.black, lineWidth: 1))
+                    
+                    VStack (alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }.navigationTitle("Resorts")
         }
     }
 }
