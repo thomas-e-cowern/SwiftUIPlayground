846880111f9de7089712362b9a74a7017e47f3ed
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-13 10:56:38 -0400

    added resort model and decodable utility to project

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,22 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Thomas Cowern New on 4/13/22.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredits: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+}
