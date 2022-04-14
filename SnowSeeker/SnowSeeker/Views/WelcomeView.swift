36465899b95282144c2e18d8ee96949b16b40677
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-14 11:02:12 -0400

    added welcome view to project

---
+++ SnowSeeker/SnowSeeker/Views/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Thomas Cowern New on 4/14/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
