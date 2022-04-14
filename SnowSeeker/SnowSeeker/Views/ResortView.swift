a488ed24ae3b08e5d6fc2e445818dc69ccbd67ce
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-14 16:00:03 -0400

    added scroll view, image and group to resort view

--- SnowSeeker/SnowSeeker/Views/ResortView.swift
+++ SnowSeeker/SnowSeeker/Views/ResortView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ResortView: View {
+    let resort: Resort
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack (alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(resort.facilities.joined(separator: ", "))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
-        ResortView()
+        ResortView(resort: Resort.example)
     }
 }
