5a8374b9dbeb9d24346ebd4bfcd561425201115e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-14 12:23:30 -0400

    added resort view

---
+++ SnowSeeker/SnowSeeker/Views/ResortView.swift
@@ -0,0 +1,20 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Thomas Cowern New on 4/14/22.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResortView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResortView()
+    }
+}
