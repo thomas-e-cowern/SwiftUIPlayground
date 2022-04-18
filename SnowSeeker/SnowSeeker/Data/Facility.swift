597291eff83942fb9e7e9c6114723a706a3dc946
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-18 09:27:37 -0400

    added facility struct

---
+++ SnowSeeker/SnowSeeker/Data/Facility.swift
@@ -0,0 +1,31 @@
+//
+//  Facility.swift
+//  SnowSeeker
+//
+//  Created by Thomas Cowern New on 4/18/22.
+//
+
+import SwiftUI
+
+struct Facility: Identifiable {
+    let id = UUID()
+    var name: String
+    
+    private let icons = [
+        "Accomodation": "house",
+        "Beginners": "1.circle",
+        "Cross-country": "map",
+        "Eco-friendly": "leaf.arrow.circlepath",
+        "Family": "person.3"
+    ]
+    
+    var icon: some View {
+        if let iconName = icons[name] {
+            return Image(systemName: iconName)
+                .accessibilityLabel(name)
+                .foregroundColor(.secondary)
+        } else {
+            fatalError("Unknown facility type \(name)")
+        }
+    }
+}
