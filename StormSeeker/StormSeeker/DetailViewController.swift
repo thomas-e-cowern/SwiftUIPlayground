03df8b98a75cc06399ecebe8fb73eb852505a696
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-21 11:16:16 -0400

    updated view controller with did select row at

--- StormSeeker/StormSeeker/DetailViewController.swift
+++ StormSeeker/StormSeeker/DetailViewController.swift
@@ -11,10 +11,13 @@ class DetailViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     
+    var selectedImage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
 
