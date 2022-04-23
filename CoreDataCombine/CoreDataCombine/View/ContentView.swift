4df8135f821628787d5866850f0ecddc100d745f
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-23 10:20:23 -0400

    added swift web image dependecy

--- CoreDataCombine/CoreDataCombine/View/ContentView.swift
+++ CoreDataCombine/CoreDataCombine/View/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @ObservedObject private var viewModel = CoinViewModel()
     @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var monitor = NetworkMonitor()
     
     @State var coins: [Coin] = []
     
@@ -21,8 +22,23 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                VStack {
+                    Image(systemName: monitor.isConnected ? "wifi" : "wifi.slash")
+                        .font(.system(size: 56))
+                    Text(monitor.isConnected ? "connected" : "not connected")
+                }
+                ScrollView {
+                    ForEach(viewModel.myCoins, id: \.self) { coin in
+                        HStack {
+                            let fixUrl = coin.iconUrl.replacingOccurrences(of: "svg", with: "png")
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- CoreDataCombine/CoreDataCombine/View/NetworkMonitor.swift
+++ CoreDataCombine/CoreDataCombine/View/NetworkMonitor.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import Network
 
-struct NetworkMonitor: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct NetworkMonitor_Previews: PreviewProvider {
-    static var previews: some View {
-        NetworkMonitor()
+final class NetworkMonitor: ObservableObject {
+    let monitor = NWPathMonitor()
+    let queue = DispatchQueue(label: "Monitor")
+    
+    @Published var isConnected = true
+    
+    init () {
+        monitor.pathUpdateHandler = {[weak self] path in
+            DispatchQueue.main.async {
+                withAnimation() {
+                    self?.isConnected = path.status == .satisfied ? true : false
+                }
+            }
+            
+        }
+        monitor.start(queue: queue)
     }
 }
