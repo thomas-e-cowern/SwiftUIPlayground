098c30a6f32b2cc983fbb42ad4966a8f254aa748
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-23 08:37:25 -0400

    core data combine initial commit

---
+++ CoreDataCombine/CoreDataCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataCombine
+//
+//  Created by Thomas Cowern New on 4/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataCombine/CoreDataCombine/CoreDataCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataCombineApp.swift
+//  CoreDataCombine
+//
+//  Created by Thomas Cowern New on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
