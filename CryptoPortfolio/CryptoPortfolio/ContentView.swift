b623ba772a7644333b3ffcadd8cf119b2f03848a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-25 09:37:43 -0400

    added list and navigation view to content view

--- CryptoPortfolio/CryptoPortfolio/ContentView.swift
+++ CryptoPortfolio/CryptoPortfolio/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(0..<10) { item in
+                Text("Hello, world!")
+                    .padding()
+            }
+            .navigationTitle("Crypto Portfolio")
+        }
     }
 }
 
