af9d5f187fc50e70d06424e2934c5376b2d8e829
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-25 10:08:09 -0400

    added coindata to coin model

--- CryptoPortfolio/CryptoPortfolio/CoinData.swift
+++ CryptoPortfolio/CryptoPortfolio/CoinData.swift
@@ -8,10 +8,29 @@
 import Foundation
 import SwiftUI
 
-struct Coin {
-    var symbol: String
-    var image: UIImage
-    var price: Double
-    var amount: Double
+class CoinData {
+    static let shared = CoinData()
+    var coins = [Coin]()
+    
+    private init () {
+        let symbols = ["BTC", "ETH", "LTC"]
+        
+        for symbol in symbols {
+            let coin = Coin(symbol: symbol)
+            coins.append(coin)
+        }
+    }
+}
+
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
     var historicalData = [Double]()
+    
+    init (symbol: String) {
+        self.symbol = symbol
+    }
 }
