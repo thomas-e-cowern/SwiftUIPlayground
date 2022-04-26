574bd313dee9e35b6caeb459cef8937b42f8f063
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-26 11:04:18 -0400

    updated content view

--- SwiftUIAPICalls/SwiftUIAPICalls/ContentView.swift
+++ SwiftUIAPICalls/SwiftUIAPICalls/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         NavigationView {
             List {
-                
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        Image("")
+                            .frame(width: 130, height: 130)
+                            .background(Color.gray)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
             }
             .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
         }
     }
 }
--- SwiftUIAPICalls/SwiftUIAPICalls/ViewModel.swift
+++ SwiftUIAPICalls/SwiftUIAPICalls/ViewModel.swift
@@ -10,12 +10,14 @@ import SwiftUI
 
 class ViewModel: ObservableObject {
     
+    @Published var courses: [Course] = []
+    
     func fetch () {
         guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else {
             return
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, _,
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _,
              error in
             guard let data = data, error == nil else {
                 return
@@ -23,10 +25,15 @@ class ViewModel: ObservableObject {
             
             do {
                 let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
             } catch {
                 print(error)
             }
         }
+        
+        task.resume()
     }
     
 }
