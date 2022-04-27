64af93cba8e79d21bef4e98c673475ce6c36769e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-27 10:38:09 -0400

    added static urls

--- GenericAPICaller/GenericAPICaller/ViewController.swift
+++ GenericAPICaller/GenericAPICaller/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct constants {
+        static let userUrl = "https://jsonplaceholder.typicode.com/users"
+        static let todoUrl = "https://jsonplaceholder.typicode.com/todos"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
