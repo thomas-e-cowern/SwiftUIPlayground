4d29a1abf4fae46e2ca0f8c1799859d556317b6e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-27 11:09:36 -0400

    added urlsession extension

--- GenericAPICaller/GenericAPICaller/ViewController.swift
+++ GenericAPICaller/GenericAPICaller/ViewController.swift
@@ -7,18 +7,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Models
+
+struct User: Codable {
+    let name: String
+    let email: String
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    struct constants {
-        static let userUrl = "https://jsonplaceholder.typicode.com/users"
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].name
+        return cell
+    }
+    
+    
+    struct Constants {
+        static let userUrl = URL(string: "https://jsonplaceholder.typicode.com/users")
         static let todoUrl = "https://jsonplaceholder.typicode.com/todos"
     }
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var users: [User] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
+        
+        fetch()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
     }
 
+    func fetch () {
+        URLSession.shared.request(url: Constants.userUrl, expecting: [User].self) { [weak self] result in
+            switch result {
+            case .success(let users):
+                DispatchQueue.main.async {
+                    self?.users = users
+                    self?.table.reloadData()
+                }
+               
+            case.failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
+extension URLSession {
+    
+    // Enums for possible errors
+    enum CustomError: Error {
+        case invalidUrl
+        case invalidData
+    }
+    
+    // Request with generic
+    func request<T: Codable>(url: URL?, expecting: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        // Check for invalid url
+        guard let  url = url else {
+            completion(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        // Check for data error
+        let task = dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(CustomError.invalidData))
+                }
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
