f384bd477210657e6e9360ba0d2eb2665504188d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-27 07:44:37 -0400

    added source model

---
+++ DictionaryTest/DictionaryTest/Source.swift
@@ -0,0 +1,13 @@
+//
+//  Source.swift
+//  DictionaryTest
+//
+//  Created by Thomas Cowern New on 4/27/22.
+//
+
+import Foundation
+
+struct Source: Codable {
+    var id: String?
+    var name: String?
+}
