ba666c958c24339b9a223c868bd6f27a36665a7c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-27 07:42:29 -0400

    added article model

---
+++ DictionaryTest/DictionaryTest/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  DictionaryTest
+//
+//  Created by Thomas Cowern New on 4/27/22.
+//
+
+import Foundation
+
+struct Article: Codable {
+    var source: Source?
+    var author: String?
+    var title: String?
+    var description: String?
+    var url: String?
+    var urlToImage: String?
+    var publishedAt: String?
+    var content: String?
+}
