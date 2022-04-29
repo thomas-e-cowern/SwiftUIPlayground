627665ada90ee6e51a0903cd01c632fd8108d477
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-04-29 10:40:40 -0400

    added line chart view

---
+++ CryptoPortfolio/CryptoPortfolio/LineChartView.swift
@@ -0,0 +1,20 @@
+//
+//  LineChartView.swift
+//  CryptoPortfolio
+//
+//  Created by Thomas Cowern New on 4/29/22.
+//
+
+import SwiftUI
+
+struct LineChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LineChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        LineChartView()
+    }
+}
