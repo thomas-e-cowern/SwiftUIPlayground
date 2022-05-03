cd3e42e9c1f67133bcf170f8fd8192b4ac164f7a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-03 09:19:38 -0400

    updated info view with text and links

--- CryptoPortfolio/CryptoPortfolio/Views/InfoView.swift
+++ CryptoPortfolio/CryptoPortfolio/Views/InfoView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct InfoView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         Form {
+            Section(header: Text("About Crypto Portfolio")) {
+                HStack {
+                    Text("Crypto Portfolio is not responsible for investing performance of any person using this app")
+                        .multilineTextAlignment(.leading)
+                }
+                HStack {
+                    Text("Crypto Portfolio uses data from coincap.io")
+                }
+                HStack {
+                    Link("Visit coincap.io", destination: URL(string: "https://coincap.io")!)
+                }
+            }
+            
+            
             Section(header: Text("Credits")) {
                 HStack {
                     Text("Developed by: Thomas E. Cowern")
                 }
+                HStack {
+                    Text("Based on a project by Nick Walter")
+                }
+                HStack {
+                    Text("Coin Images : https://github.com/spothq/cryptocurrency-icons")
+                }
+                HStack (alignment: .center) {
+                    Spacer()
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                    Spacer()
+                }
             }
         }
+        
+        
+        
+        Spacer()
     }
 }
 
