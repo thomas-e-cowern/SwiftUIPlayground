bbdecdfd3e80b139d76911824e53550196b83758
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-23 15:37:36 -0400

    added book inventory project

---
+++ BookInventory/BookInventory/BookInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookInventoryApp.swift
+//  BookInventory
+//
+//  Created by Thomas Cowern New on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BookInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookInventory/BookInventory/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  BookInventory
+//
+//  Created by Thomas Cowern New on 5/23/22.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @State var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
