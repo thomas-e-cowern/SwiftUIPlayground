620fdf50e3fc164fe4e2ebbdf6fc9d5e8b978567
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-23 19:25:21 -0400

    added student model

--- BookInventory/BookInventory/ContentView.swift
+++ BookInventory/BookInventory/ContentView.swift
@@ -7,33 +7,26 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    
-    @State private var rememberMe = false
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
     var body: some View {
         VStack {
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+            List(students) { student in
+                Text(student.name ?? "Unknown Student")
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Moskovits", "Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()
+                let chosenLastName = lastNames.randomElement()
+                
+                
+                
+            }
         }
     }
 }
---
+++ BookInventory/BookInventory/DataController.swift
@@ -0,0 +1,23 @@
+//
+//  DataController.swift
+//  BookInventory
+//
+//  Created by Thomas Cowern New on 5/23/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    
+    let container = NSPersistentContainer(name: "BookInventory")
+    
+    init () {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
