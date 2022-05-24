fd082b8b060b6133359603db0c0b50bc5860b897
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-24 16:11:54 -0400

    reset model and content view

--- BookInventory/BookInventory/ContentView.swift
+++ BookInventory/BookInventory/ContentView.swift
@@ -9,28 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+//    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
     var body: some View {
         VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown Student")
-            }
-            
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Moskovits", "Granger", "Lovegood", "Potter", "Weasley"]
-                
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-                
-            }
+            Text("Start Over")
         }
     }
 }
