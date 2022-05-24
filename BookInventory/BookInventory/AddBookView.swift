a410c854fd339aa1b19e13259e97df98906ef743
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-24 16:37:30 -0400

    added dismiss environment variable to add new book veiw

--- BookInventory/BookInventory/AddBookView.swift
+++ BookInventory/BookInventory/AddBookView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddBookView: View {
     
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
     
     @State private var title: String = ""
     @State private var author: String = ""
@@ -57,6 +58,7 @@ struct AddBookView: View {
                         newBook.review = review
                         
                         try? moc.save()
+                        dismiss()
                     }
                 }
             }
