060c032d2df6555b5e9c51ea70797ec34ce8dba9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-25 09:03:19 -0400

    added scroll view, nav bar, and image to detail view

--- BookInventory/BookInventory/DetailView.swift
+++ BookInventory/BookInventory/DetailView.swift
@@ -13,7 +13,24 @@ struct DetailView: View {
     let book: Book
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack (alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
