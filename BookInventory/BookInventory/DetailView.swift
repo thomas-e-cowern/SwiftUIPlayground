be24ed774e0d763335c41f6b6f6c1a8c17b9c2cf
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-25 08:57:54 -0400

    added detail view

---
+++ BookInventory/BookInventory/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  BookInventory
+//
+//  Created by Thomas Cowern New on 5/25/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    
+    let book: Book
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
