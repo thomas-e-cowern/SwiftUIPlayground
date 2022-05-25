3041e1be030ad477d81ec7a9474428296402d8e6
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-05-25 08:59:26 -0400

    added static moc and book to preview in detail view

--- BookInventory/BookInventory/DetailView.swift
+++ BookInventory/BookInventory/DetailView.swift
@@ -18,7 +18,18 @@ struct DetailView: View {
 }
 
 struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
-    }
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        static var previews: some View {
+            let book = Book(context: moc)
+            book.title = "Test book"
+            book.author = "Test author"
+            book.genre = "Fantasy"
+            book.rating = 4
+            book.review = "This was a great book; I really enjoyed it."
+
+            return NavigationView {
+                DetailView(book: book)
+            }
+        }
 }
