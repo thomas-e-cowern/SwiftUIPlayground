39514e59ff04f2193b364a435440b0256468fa64
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-11 10:35:53 -0400

    added codable networking project to directory

---
+++ CodableNetworking/CodableNetworking/CodableNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableNetworkingApp.swift
+//  CodableNetworking
+//
+//  Created by Thomas Cowern New on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CodableNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
