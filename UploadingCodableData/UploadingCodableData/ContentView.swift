60d5ee271482d9f5e6458126d80485e0113ea3c9
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-12 14:56:42 -0400

    updated files

--- UploadingCodableData/UploadingCodableData/ContentView.swift
+++ UploadingCodableData/UploadingCodableData/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     
@@ -39,12 +40,26 @@ struct ContentView: View {
 //        }.resume()
 //    }
     
-    func upload<Input: Encodable, Output: Decodable>(
-        _ data: Input, to url: URL,
-        httpMethod: String = "POST",
-        contenttype: String = "application/json",
-        completion: @escaping (Result<Output, Error>) -> Void) {
-        // More to come
+    func upload<Input: Encodable, Output: Decodable>(_ data: Input, to url: URL, httpMethod: String = "POST", contentType: String = "application/json", completion: @escaping (Result<Output, UploadError>) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+
+        let encoder = JSONEncoder()
+        request.httpBody = try? encoder.encode(data)
+
+        URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: Output.self, decoder: JSONDecoder())
+            .map(Result.success)
+            .catch { error -> Just<Result<Output, UploadError>> in
+                error is DecodingError
+                    ? Just(.failure(.decodeFailed))
+                    : Just(.failure(.uploadFailed))
+            }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: completion)
+            .store(in: &requests)
     }
 }
 
