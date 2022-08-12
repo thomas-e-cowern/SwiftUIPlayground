241fd1aebaf829cb3c2017757a3d1b4fe87bedd2
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-12 11:46:09 -0400

    added upload method to movie star

--- UploadingCodableData/UploadingCodableData/MovieStar.swift
+++ UploadingCodableData/UploadingCodableData/MovieStar.swift
@@ -10,4 +10,24 @@ import Foundation
 struct MovieStar: Codable {
     let name: String
     let movies: [String]
+    
+    func upload(_ data: MovieStar, to url: URL) {
+        let encoder = JSONEncoder()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? encoder.encode(data)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                let result = String(decoding: data, as: UTF8.self)
+                print(result)
+            } else if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("There was an unidentified error")
+            }
+        }.resume()
+    }
 }
