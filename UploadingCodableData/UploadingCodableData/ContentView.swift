860d0b0f86cb6711e5ce8eb49b3102960a44953e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-12 11:22:14 -0400

    added uploading codable data projct

---
+++ UploadingCodableData/UploadingCodableData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UploadingCodableData
+//
+//  Created by Thomas Cowern New on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UploadingCodableData/UploadingCodableData/UploadingCodableDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  UploadingCodableDataApp.swift
+//  UploadingCodableData
+//
+//  Created by Thomas Cowern New on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UploadingCodableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
