8a4275bdf5de1f14405a2986ae2946485df79e1a
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-12 11:37:17 -0400

    added movie star model to uploading codable data

---
+++ UploadingCodableData/UploadingCodableData/MovieStar.swift
@@ -0,0 +1,13 @@
+//
+//  MovieStar.swift
+//  UploadingCodableData
+//
+//  Created by Thomas Cowern New on 8/12/22.
+//
+
+import Foundation
+
+struct MovieStar: Codable {
+    let name: String
+    let movies: [String]
+}
