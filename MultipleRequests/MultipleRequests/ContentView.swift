c43d845d7737f286b939b35d8bbeb17666ee825e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-15 13:31:35 -0400

    added fetch method to content view

--- MultipleRequests/MultipleRequests/ContentView.swift
+++ MultipleRequests/MultipleRequests/ContentView.swift
@@ -42,6 +42,20 @@ struct ContentView: View {
             }
             .navigationTitle("Messages")
         }
+    }
+    
+    func fetch<T: Decodable>(_ url: URL, defaultValue: T, completion: @escaping (T) -> Void) {
+        let decoder = JSONDecoder()
+
+        URLSession.shared.dataTaskPublisher(for: url)
+            .retry(1)
+            .map(\.data)
+            .decode(type: T.self, decoder: decoder)
+            .replaceError(with: defaultValue)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: completion)
+            .store(in: &requests)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
