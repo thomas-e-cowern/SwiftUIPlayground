cc0c63b50dcfe748b60bb78fac123f02a147701b
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-15 11:19:45 -0400

    added multiple requests project

---
+++ MultipleRequests/MultipleRequests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultipleRequests
+//
+//  Created by Thomas Cowern New on 8/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultipleRequests/MultipleRequests/MultipleRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleRequestsApp.swift
+//  MultipleRequests
+//
+//  Created by Thomas Cowern New on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MultipleRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
