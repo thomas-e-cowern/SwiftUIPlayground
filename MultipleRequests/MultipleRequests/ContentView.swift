4fd9c4cfd01a3bbf8dbde189e65efad9ba122095
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-08-15 15:44:25 -0400

    added second fetch mehtod to content view

--- MultipleRequests/MultipleRequests/ContentView.swift
+++ MultipleRequests/MultipleRequests/ContentView.swift
@@ -71,6 +71,18 @@ struct ContentView: View {
             .sink(receiveValue: completion)
             .store(in: &requests)
     }
+    
+    func fetch<T: Decodable>(_ url: URL, defaultValue: T) -> AnyPublisher<T, Never> {
+        let decoder = JSONDecoder()
+
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .retry(1)
+            .map(\.data)
+            .decode(type: T.self, decoder: decoder)
+            .replaceError(with: defaultValue)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
