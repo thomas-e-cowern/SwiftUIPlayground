2a548f10de2e4a37c756615e33609c7d0df774f8
Thomas Cowern <thomascowern@thomass-air.attlocal.net>
2022-08-17 06:35:23 -0400

    added cupcakes networking chained requests start

---
+++ Cupcakes/networking-chained-requests-start/ChainedNetworking/ChainedNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChainedNetworkingApp.swift
+//  ChainedNetworking
+//
+//  Created by Paul Hudson on 23/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct ChainedNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cupcakes/networking-chained-requests-start/ChainedNetworking/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//
+
+import Combine
+import SwiftUI
+
+struct NewsItem: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let strap: String
+    let url: URL
+    let main_image: String
+    let published_date: Date
+}
+
+struct ContentView: View {
+    @State private var requests = Set<AnyCancellable>()
+    @State private var items = [NewsItem]()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Fetch news") {
+                    // fetch the news!
+                }
+
+                List(items) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.title)
+                            .font(.headline)
+                        Text(item.strap)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("Hacking with Swift")
+        }
+    }
+
+    func fetch<T: Decodable>(_ url: URL, defaultValue: T) -> AnyPublisher<T, Never> {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .retry(1)
+            .map(\.data)
+            .decode(type: T.self, decoder: decoder)
+            .replaceError(with: defaultValue)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
