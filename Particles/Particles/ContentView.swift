04fc00aa9f99d45946fb294874acaa146dfcbc72
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-08-18 13:17:37 -0400

    added particles project and folder

---
+++ Particles/Particles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Particles
+//
+//  Created by Paul Hudson on 07/06/2020.
+//  Copyright © 2020 Paul Hudson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
