3abbffecf14e8273784abf37c1e259b0c9ffc58d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 11:22:11 -0400

    added networking test to playground

---
+++ NetworkingTest/NetworkingTest/NetworkingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTestApp.swift
+//  NetworkingTest
+//
+//  Created by Thomas Cowern on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
