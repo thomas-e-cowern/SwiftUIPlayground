031aab22f44765c11d4cb6f798fd30bfa4f6f798
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 12:35:32 -0400

    updated fetch users in users view model

--- NetworkingTest/NetworkingTest/UsersViewModel.swift
+++ NetworkingTest/NetworkingTest/UsersViewModel.swift
@@ -8,16 +8,27 @@
 import Foundation
 
 final class UsersViewModel: ObservableObject {
+    
+    @Published var users: [User] = []
+    
     func fetchUsers() {
         
         let userUrlString = "https://jsonplaceholder.typicode.com/users"
         if let url = URL(string: userUrlString) {
-            URLSession.shared.dataTask(with: url) { data, response, error in
+            URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
                 if let error = error {
                     print("There was an error in the network call: \(error.localizedDescription)")
                 } else {
                     let decoder = JSONDecoder()
                     decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    
+                    if let data = data,
+                        let users = try? decoder.decode([User].self, from: data) {
+                        self?.users = users
+                        } else {
+                            print("There was a problem decoding json: \(error?.localizedDescription ?? "Decode error")")
+                        }
+                    
                 }
             }.resume()
         }
