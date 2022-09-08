99693f779665e7f2fb1d4dca59a91837e4d9c342
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 12:26:29 -0400

    added fetch users method to users view model

--- NetworkingTest/NetworkingTest/UsersViewModel.swift
+++ NetworkingTest/NetworkingTest/UsersViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class UsersViewModel: ObservableObject {
+    func fetchUsers() {
+        
+        let userUrlString = "https://jsonplaceholder.typicode.com/users"
+        if let url = URL(string: userUrlString) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("There was an error in the network call: \(error.localizedDescription)")
+                } else {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                }
+            }.resume()
+        }
+    }
+}
