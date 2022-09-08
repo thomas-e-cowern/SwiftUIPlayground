f75e03e125116978c4127c17a94aa5403d5c17d8
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 13:03:56 -0400

    added custom error to users view model

--- NetworkingTest/NetworkingTest/UsersViewModel.swift
+++ NetworkingTest/NetworkingTest/UsersViewModel.swift
@@ -10,8 +10,12 @@ import Foundation
 final class UsersViewModel: ObservableObject {
     
     @Published var users: [User] = []
+    @Published var hasError = false
+    @Published var error: UserError?
     
     func fetchUsers() {
+        
+        hasError = false
         
         let userUrlString = "https://jsonplaceholder.typicode.com/users"
         if let url = URL(string: userUrlString) {
@@ -20,6 +24,8 @@ final class UsersViewModel: ObservableObject {
                 DispatchQueue.main.async {
                     if let error = error {
                         print("There was an error in the network call: \(error.localizedDescription)")
+                        self?.hasError = true
+                        self?.error = UserError.custom(error: error)
                     } else {
                         let decoder = JSONDecoder()
                         decoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -29,11 +35,29 @@ final class UsersViewModel: ObservableObject {
                             self?.users = users
                             } else {
                                 print("There was a problem decoding json: \(error?.localizedDescription ?? "Decode error")")
+                                self?.hasError = true
+                                self?.error = UserError.failedToDecode
                             }
                     }
                 }
 
             }.resume()
+        }
+    }
+}
+
+extension UsersViewModel {
+    enum UserError: LocalizedError {
+        case custom(error: Error)
+        case failedToDecode
+        
+        var errorDescription: String? {
+            switch self {
+            case .failedToDecode:
+                return "Failed to decode response"
+            case .custom(let error):
+                return error .localizedDescription
+            }
         }
     }
 }
