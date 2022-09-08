a85e1c0b0593899dca774701eac30c9956e46f7a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-08 11:36:52 -0400

    added user and company structs to user model

--- NetworkingTest/NetworkingTest/UserModel.swift
+++ NetworkingTest/NetworkingTest/UserModel.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 struct User: Codable {
-    
+    let id: Int
+    let name: String
+    let email: String
+    let company: Company
+}
+
+struct Company: Codable {
+    let name: String
 }
