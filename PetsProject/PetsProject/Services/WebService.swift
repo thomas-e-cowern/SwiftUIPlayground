05484cc8b76afd847e4cfb3537c8334f9c9e16ea
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-12 13:19:41 -0400

    update web service with json decoder

--- PetsProject/PetsProject/Services/WebService.swift
+++ PetsProject/PetsProject/Services/WebService.swift
@@ -14,7 +14,7 @@ enum NetworkError: Error {
 
 class WebService {
     
-    func load(resource: String) async throws {
+    func load(resource: String) async throws -> ScreenModel{
         
         guard let url = URL(string: resource) else {
             throw NetworkError.invalidUrl
@@ -22,9 +22,11 @@ class WebService {
         
         let (data, response) = try await URLSession.shared.data(from: url)
         
-        guard let httpResponse = resource as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
             throw NetworkError.invalidServerResponse
         }
+        
+        return try JSONDecoder().decode(ScreenModel.self, from: data)
     }
     
 }
