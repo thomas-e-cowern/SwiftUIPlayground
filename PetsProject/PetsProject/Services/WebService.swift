70cca327e5fe9c93256b6dc13b3b194df6c6b74f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-12 12:31:49 -0400

    added services folder and web service file and class

---
+++ PetsProject/PetsProject/Services/WebService.swift
@@ -0,0 +1,16 @@
+//
+//  WebService.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/12/22.
+//
+
+import Foundation
+
+class WebService {
+    
+    func load() {
+        
+    }
+    
+}
