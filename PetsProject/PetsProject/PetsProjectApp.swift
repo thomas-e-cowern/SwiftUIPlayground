defe3952e5feee7cd26e4eb6fc65036418926c38
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-12 12:29:28 -0400

    pets project initial commit

---
+++ PetsProject/PetsProject/PetsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsProjectApp.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PetsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
