91246610e40bba392a799e98eb70bba1c6535ad6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-16 10:23:51 -0400

    updated constants with screen resources struct

--- PetsProject/PetsProject/Utilities/Constants.swift
+++ PetsProject/PetsProject/Utilities/Constants.swift
@@ -9,6 +9,19 @@ import Foundation
 
 struct Constants {
     
+    struct ScreenResources {
+        static let baseUrl = "localhost:3000"
+        static let petListing = "pet-listing"
+        
+        static func resource(for resourceName: String) -> URL? {
+            var components = URLComponents()
+            components.scheme = "http"
+            components.percentEncodedHost = baseUrl
+            components.path = "/\(resourceName)"
+            return components.url
+        }
+    }
+    
     struct Urls {
         static let baseUrl = "http://localhost:3000/"
         static let petListing = "\(baseUrl)pet-listing"
