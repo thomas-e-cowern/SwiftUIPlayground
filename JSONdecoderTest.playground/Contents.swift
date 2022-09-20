4946fca4ab288fe004316c2a9c84486ecbd8e568
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-20 13:00:25 -0400

    updated init in playground for testing

--- JSONdecoderTest.playground/Contents.swift
+++ JSONdecoderTest.playground/Contents.swift
@@ -11,23 +11,43 @@ let json = """
 
 struct JSON: Decodable {
     
+    var value: Any
+    
     private struct CodingKeys: CodingKey {
         var stringValue: String
         
         init?(stringValue: String) {
-            <#code#>
+            self.stringValue = stringValue
         }
         
         var intValue: Int?
         
         init?(intValue: Int) {
-            <#code#>
+            self.stringValue = "\(intValue)"
+            self.intValue = intValue
         }
-        
-        
     }
     
     init(from decoder: Decoder) throws {
+        
+        if let container = try? decoder.container(keyedBy: CodingKeys.self) {
+            var result = [String: Any]()
+            for key in container.allKeys {
+                result[key.stringValue] = try container.decode(JSON.self, forKey: key).value
+            }
+            value = result
+        } else if let container = try?  decoder.singleValueContainer() {
+            if let stringValue = try? container.decode(String.self) {
+                value = stringValue
+            } else if let intValue = try? container.decode(Int.self) {
+                value = intValue
+            } else if let boolValue = try? container.decode(Bool.self) {
+                value = boolValue
+            } else if let arrayValue = try? container.decode([JSON].self) {
+                value = arrayValue.map { $0.value }
+            }
+            
+        }
         
     }
 }
