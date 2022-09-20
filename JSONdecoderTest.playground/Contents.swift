3a44881e3dbaa08a75bc771dac86e44d4188b558
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-20 12:50:40 -0400

    added json struct and coding keys to playground for testing purposes

--- JSONdecoderTest.playground/Contents.swift
+++ JSONdecoderTest.playground/Contents.swift
@@ -8,3 +8,26 @@ let json = """
     "isVisible": true
 }
 """
+
+struct JSON: Decodable {
+    
+    private struct CodingKeys: CodingKey {
+        var stringValue: String
+        
+        init?(stringValue: String) {
+            <#code#>
+        }
+        
+        var intValue: Int?
+        
+        init?(intValue: Int) {
+            <#code#>
+        }
+        
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+    }
+}
