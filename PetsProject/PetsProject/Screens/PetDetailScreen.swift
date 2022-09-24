1b72a8dc43d23b0bf11d98f89188bccfae3898d6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-24 16:07:03 -0400

    added pet detail screen

---
+++ PetsProject/PetsProject/Screens/PetDetailScreen.swift
@@ -0,0 +1,20 @@
+//
+//  PetDetailScreen.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/24/22.
+//
+
+import SwiftUI
+
+struct PetDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PetDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PetDetailScreen()
+    }
+}
