37c46bb1baf7687dcc28da73fc3cae4ed72e3ce8
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-27 12:57:19 -0400

    updated rating row ccomponent and screen model

--- PetsProject/PetsProject/Components/RatingRowComponent.swift
+++ PetsProject/PetsProject/Components/RatingRowComponent.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RatingRowComponent: UIComponent {
+    
+    let uiModel: RatingRowUIModel
+     
+    let id = UUID()
+    
+    func render() -> AnyView {
+        Text("Rating \(uiModel.rating)").toAnyView()
+    }
+}
--- PetsProject/PetsProject/UIModels/ScreenModel.swift
+++ PetsProject/PetsProject/UIModels/ScreenModel.swift
@@ -15,6 +15,7 @@ enum ComponentType: String, Decodable {
     case featuredImage
     case carousel
     case textRow
+    case ratingRow
 }
 
 struct ComponentModel: Decodable {
@@ -64,6 +65,11 @@ extension ScreenModel {
                 }
                 components.append(TextRowComponent(uiModel: uiModel))
                 
+            case .ratingRow:
+                guard let uiModel: RatingRowUIModel = component.data.decode() else {
+                    throw ComponentError.decodingError
+                }
+                components.append(RatingRowComponent(uiModel: uiModel))
             }
         }
         
