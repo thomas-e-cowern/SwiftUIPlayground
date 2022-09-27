b4e72421433c74faad6d7fb3f37f7a8536bf0d3b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-27 12:45:08 -0400

    added rating row ui model to project

---
+++ PetsProject/PetsProject/UIModels/RatingRowUIModel.swift
@@ -0,0 +1,12 @@
+//
+//  RatingRowUIModel.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/27/22.
+//
+
+import Foundation
+
+struct RatingRowUIModel: Decodable {
+    let rating: Int
+}
