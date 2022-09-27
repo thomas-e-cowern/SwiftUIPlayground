4c308702e3ee025a9e4c4d97629a7a75d5b7c670
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-27 10:51:24 -0400

    added text row component

---
+++ PetsProject/PetsProject/Components/TextRowComponent.swift
@@ -0,0 +1,20 @@
+//
+//  TextRowComponent.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/27/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct TextRowComponent: UIComponent {
+    
+    var id: UUID = UUID()
+
+    let uiModel: TextRowUIModel
+    
+    func render() -> AnyView {
+        Text("Text Row").toAnyView()
+    }
+}
--- PetsProject/PetsProject/UIModels/TextRowUIModel.swift
+++ PetsProject/PetsProject/UIModels/TextRowUIModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct TextRowUIModel: Decodable {
+    let text: String
+}
