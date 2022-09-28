caf45bf0eb26f08c17c01385bac70ec83227aba0
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-09-28 09:11:11 -0400

    added list ui model to ui models folder

---
+++ PetsProject/PetsProject/UIModels/ListUIModel.swift
@@ -0,0 +1,21 @@
+//
+//  ListUIModel.swift
+//  PetsProject
+//
+//  Created by Thomas Cowern on 9/28/22.
+//
+
+import Foundation
+
+struct RowUIModel: Decodable {
+    let id: Int
+    let title: String
+    let subTitle: String?
+    let imageUrl: URL?
+    let description: String?
+}
+
+struct ListUIModel: Decodable {
+    let rows: [RowUIModel]
+    let action: Action
+}
