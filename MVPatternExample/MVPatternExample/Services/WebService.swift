1fa7d15f92a017fa558f923d0efa21bfbc8b21b4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-03 16:20:13 -0400

    added network error enum and get products method to web service

--- MVPatternExample/MVPatternExample/Services/WebService.swift
+++ MVPatternExample/MVPatternExample/Services/WebService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case badUrl
+    case badRequest
+    case decodingError
+}
+
+class WebService {
+    func getProducts() async throws -> [Product] {
+            
+        guard let url = URL(string: "https://fakestoreapi.com/products") else {
+            throw NetworkError.badUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+                
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let products = try? JSONDecoder().decode([Product].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return products
+    }
+    
+}
