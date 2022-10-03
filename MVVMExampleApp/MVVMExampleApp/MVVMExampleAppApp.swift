69d446858d6d1c8f5c2e1695ee063951f6800006
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-03 13:10:04 -0400

    mvvmexampleapp initial commit

---
+++ MVVMExampleApp/MVVMExampleApp/MVVMExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleAppApp.swift
+//  MVVMExampleApp
+//
+//  Created by Thomas Cowern on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
