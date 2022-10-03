0abdb6aab236bc0e4e118dbc80f2cc0801e0455a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-03 15:50:44 -0400

    added product model

---
+++ MVPatternExample/MVPatternExample/Models/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  MVPatternExample
+//
+//  Created by Thomas Cowern on 10/3/22.
+//
+
+import Foundation
+
+struct Product: Codable {
+    var id: Int
+    var title: String
+    var price: Double
+}
