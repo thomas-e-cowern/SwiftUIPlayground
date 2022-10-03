c45495f60b07b3c1d2ef99d86da198228d671676
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-03 13:52:04 -0400

    added product list view class with init and populate products method

--- MVVMExampleApp/MVVMExampleApp/ViewModels/ProductListViewModel.swift
+++ MVVMExampleApp/MVVMExampleApp/ViewModels/ProductListViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class ProductListViewModel: ObservableObject {
+    
+    let webService: WebService
+    
+    init(webService: WebService) {
+        self.webService = webService
+    }
+    
+    func populateProducts() async {
+        
+        do {
+            let products = try await webService.getProducts()
+        } catch {
+            print("Error in populate products \(error.localizedDescription)")
+        }
+    }
+    
+}
