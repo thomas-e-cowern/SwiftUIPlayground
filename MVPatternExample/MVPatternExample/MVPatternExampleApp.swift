6f6445a8c865430e4a11f285483614d469823e9e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-03 15:45:35 -0400

    mv pattern example initial commit

---
+++ MVPatternExample/MVPatternExample/MVPatternExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPatternExampleApp.swift
+//  MVPatternExample
+//
+//  Created by Thomas Cowern on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MVPatternExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
