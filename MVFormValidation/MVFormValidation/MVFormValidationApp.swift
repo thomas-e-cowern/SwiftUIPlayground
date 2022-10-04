b537d1da75000478b881bb8a5702c4f9912f8764
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-04 13:55:35 -0400

    mv form validation initial commit

---
+++ MVFormValidation/MVFormValidation/MVFormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVFormValidationApp.swift
+//  MVFormValidation
+//
+//  Created by Thomas Cowern on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MVFormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
