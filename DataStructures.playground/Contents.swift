2af3e8eb064b6be18bce2b910cda5a3b0d505566
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-04 09:56:49 -0400

    added array literal initializer

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -14,16 +14,25 @@ final class LinkedListNode<Element> {
 }
 
 // LiST
-final class LinkedList<Element> {
+final class LinkedList<Element>: ExpressibleByArrayLiteral {
+    
     var start: LinkedListNode<Element>?
+    
+    init(arrayLiteral elements: Element...) {
+        for element in elements.reversed() {
+            start = LinkedListNode(value: element, next:  start)
+        }
+    }
 }
 
-let third = LinkedListNode(value: 5)
-let second = LinkedListNode(value: 3, next: third)
-let first = LinkedListNode(value: 1, next: second)
-let list = LinkedList<Int>()
+//let third = LinkedListNode(value: 5)
+//let second = LinkedListNode(value: 3, next: third)
+//let first = LinkedListNode(value: 1, next: second)
+//let list = LinkedList<Int>()
 
-list.start = first
+//list.start = first
+
+let list: LinkedList<Int> = [2, 4, 6]
 
 var currentNode = list.start
 
