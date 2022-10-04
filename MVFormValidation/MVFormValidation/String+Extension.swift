7e15a17353db38dde87860c6f16abeb33db5f29f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-04 14:18:08 -0400

    updated string extension with is valid email

--- MVFormValidation/MVFormValidation/String+Extension.swift
+++ MVFormValidation/MVFormValidation/String+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension String {
+    
+    var isValidEmail: Bool {
+        let emailRegex = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,63}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: self)
+    }
+    
+}
