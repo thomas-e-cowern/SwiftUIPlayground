e748efa1939f3be4f89383e3dfe980d8e1827767
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-04 09:50:28 -0400

    added node details and initialized list.  Works

--- DataStructures.playground/Contents.swift
+++ DataStructures.playground/Contents.swift
@@ -18,5 +18,17 @@ final class LinkedList<Element> {
     var start: LinkedListNode<Element>?
 }
 
+let third = LinkedListNode(value: 5)
+let second = LinkedListNode(value: 3, next: third)
+let first = LinkedListNode(value: 1, next: second)
+let list = LinkedList<Int>()
 
+list.start = first
+
+var currentNode = list.start
+
+while let node = currentNode {
+    print(node.value, terminator: " ")
+    currentNode = node.next
+}
 
