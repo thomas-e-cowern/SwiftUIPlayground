46c6ef78ce7d8bb64411b8aa0223922b5c09074f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-04 14:02:55 -0400

    added text fields and button to content view

--- MVFormValidation/MVFormValidation/ContentView.swift
+++ MVFormValidation/MVFormValidation/ContentView.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email: String?
+    @State var password: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            TextField("Email", text: $email)
+            TextField("Password", text: $password)
+            Button("Login") {
+                
+            }.disabled(true)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(email: "Email", password: "Password")
     }
 }
