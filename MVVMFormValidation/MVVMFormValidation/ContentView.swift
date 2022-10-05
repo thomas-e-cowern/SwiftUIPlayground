e218175ae61797ffb6a92e1bfae1670786bacb8d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-05 10:06:50 -0400

    add email and password variables to content view

--- MVVMFormValidation/MVVMFormValidation/ContentView.swift
+++ MVVMFormValidation/MVVMFormValidation/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
