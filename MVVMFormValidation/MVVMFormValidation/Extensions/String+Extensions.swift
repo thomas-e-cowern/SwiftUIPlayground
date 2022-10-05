94acb4366c57693874bc9c35da2a0c88a053cbe4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-05 10:30:07 -0400

    add extensions group and string+extensions file with isValidEmail

---
+++ MVVMFormValidation/MVVMFormValidation/Extensions/String+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extensions.swift
+//  MVVMFormValidation
+//
+//  Created by Thomas Cowern on 10/5/22.
+//
+
+import Foundation
+
+extension String {
+    
+    var isValidEmail: Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: self)
+    }
+    
+}
