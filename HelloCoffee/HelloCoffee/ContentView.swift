a96665b9476a07044698a9ac4dff46d6aa314c0a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 12:22:41 -0400

    add populate order method to content view

--- HelloCoffee/HelloCoffee/ContentView.swift
+++ HelloCoffee/HelloCoffee/ContentView.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject private var model: CoffeeModel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(model.orders) { order in
+                Text(order.name)
+            }
         }
         .padding()
+        .task {
+            await populateOrders()
+        }
+    }
+    
+    private func populateOrders() async {
+        do {
+            try await model.populateOrders()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(CoffeeModel(webService: WebService()))
     }
 }
