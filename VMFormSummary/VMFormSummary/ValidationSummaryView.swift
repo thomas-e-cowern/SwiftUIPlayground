6315c0814619859c829f0498a37d8c1adba479bd
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 10:43:45 -0400

    add validtion summary view

---
+++ VMFormSummary/VMFormSummary/ValidationSummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  ValidationSummaryView.swift
+//  VMFormSummary
+//
+//  Created by Thomas Cowern on 10/6/22.
+//
+
+import SwiftUI
+
+struct ValidationSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ValidationSummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ValidationSummaryView()
+    }
+}
