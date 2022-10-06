55f4cdf48e4bf1c0e70f2e0167c8dc3928259021
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 12:07:49 -0400

    update web service with get orders method

--- HelloCoffee/HelloCoffee/Services/WebService.swift
+++ HelloCoffee/HelloCoffee/Services/WebService.swift
@@ -7,10 +7,32 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case badUrl
+    case badRequest
+    case decodingError
+}
+
 class WebService {
     
     func getOrders() async throws -> [Order] {
+        // https://island-bramble/glitch.me/test/orders
         
+        guard let url = URL(string: "https://island-bramble/glitch.me/test/orders") else {
+            throw NetworkError.badUrl
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse,
+            httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let orders = try? JSONDecoder().decode([Order].self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return orders
     }
     
 }
