90ba55733216a4c5656baf339a89b582ebc35969
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 12:17:16 -0400

    update hello coffee app with web service and model

--- HelloCoffee/HelloCoffee/HelloCoffeeApp.swift
+++ HelloCoffee/HelloCoffee/HelloCoffeeApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct HelloCoffeeApp: App {
+    
+    @StateObject private var model: CoffeeModel
+    
+    init() {
+        let webService = WebService()
+        _model = StateObject(wrappedValue: CoffeeModel(webService: webService))
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
         }
     }
 }
