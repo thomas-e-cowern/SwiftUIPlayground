5ddc832efd7343dd80834707e62692a2b8a6f85c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 12:13:31 -0400

    update coffe model with initializer and populate orders method

--- HelloCoffee/HelloCoffee/Models/CoffeeModel.swift
+++ HelloCoffee/HelloCoffee/Models/CoffeeModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+@MainActor
+class CoffeeModel: ObservableObject {
+    
+    let webService: WebService
+    @Published private(set) var orders: [Order] = []
+    
+    init(webService: WebService) {
+        self.webService = webService
+    }
+    
+    func populateOrders() async throws {
+        orders = try await webService.getOrders()
+    }
+    
+}
