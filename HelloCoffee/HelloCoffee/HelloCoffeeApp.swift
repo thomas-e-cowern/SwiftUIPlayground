9cf259cae4df3c8903f5714dfd93aaff8c3c5da4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 11:20:46 -0400

    hello coffe initial commit

---
+++ HelloCoffee/HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Thomas Cowern on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
