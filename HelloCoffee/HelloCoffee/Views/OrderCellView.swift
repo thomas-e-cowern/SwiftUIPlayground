e40ff66a3c02673662c8b67200c4808ad7d31ad9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 12:48:43 -0400

    updated orcer cell view with text

--- HelloCoffee/HelloCoffee/Views/OrderCellView.swift
+++ HelloCoffee/HelloCoffee/Views/OrderCellView.swift
@@ -12,7 +12,19 @@ struct OrderCellView: View {
     let order: Order
     
     var body: some View {
-        Text(order.name)
+        HStack {
+            VStack (alignment: .leading) {
+                Text(order.name)
+                    .accessibilityIdentifier("orderNameText")
+                    .bold()
+                Text("\(order.coffeeName) (\(order.size.rawValue))")
+                    .accessibilityIdentifier("coffeNameAndSizeText")
+                    .opacity(0.5)
+            }
+            Spacer()
+            Text(order.total as NSNumber, formatter: NumberFormatter.currency)
+                .accessibilityIdentifier("coffePriceText")
+        }
     }
 }
 
