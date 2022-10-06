cdf2d042ca55b432ef2282ff8cf8f4f7f5419b43
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-06 10:14:30 -0400

    vm form summary initial commit

---
+++ VMFormSummary/VMFormSummary/VMFormSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VMFormSummaryApp.swift
+//  VMFormSummary
+//
+//  Created by Thomas Cowern on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct VMFormSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
