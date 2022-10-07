2fcfd5b7ea0075dea47f8b3e947233a25e27734c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-07 14:00:37 -0400

    add end to end ui tests to project

---
+++ HelloCoffee/HelloCoffeeUIEndToEndTests/HelloCoffeeUIEndToEndTests.swift
@@ -0,0 +1,19 @@
+//
+//  HelloCoffeeUIEndToEndTests.swift
+//  HelloCoffeeUIEndToEndTests
+//
+//  Created by Thomas Cowern on 10/7/22.
+//
+
+import XCTest
+
+final class HelloCoffeeUIEndToEndTests: XCTestCase {
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
