b637a1f3f2f48600e74e04bb07226cfe8b7b8ded
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-07 10:57:40 -0400

    update app environment with base url computed variable

--- HelloCoffee/HelloCoffee/Utilities/AppEnvironment.swift
+++ HelloCoffee/HelloCoffee/Utilities/AppEnvironment.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum AppEnvironment: String {
+    case dev
+    case test
+    
+    var baseUrl: URL {
+        
+        switch self {
+        case .dev:
+            return URL(string: "https://island-bramble.glitch.me/orders")!
+
+        case .test:
+            return URL(string: "https://island-bramble.glitch.me/orders")!
+        }
+    }
+}
