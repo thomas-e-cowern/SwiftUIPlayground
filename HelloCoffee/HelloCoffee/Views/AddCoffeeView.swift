99f7b6abb24b6c88d608fa5a6a1d51c626c18c86
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-10 15:02:36 -0400

    add variables to add coffee view

--- HelloCoffee/HelloCoffee/Views/AddCoffeeView.swift
+++ HelloCoffee/HelloCoffee/Views/AddCoffeeView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct AddCoffeeView: View {
+    
+    @State private var name: String = ""
+    @State private var coffeeName: String = ""
+    @State private var price: String = ""
+    @State private var coffeeSize: String = ""
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
