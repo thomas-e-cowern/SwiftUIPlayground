aca648d4009d12ac8df88875f6d52e4446d9e758
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-10 15:00:01 -0400

    update web service place order complete

--- HelloCoffee/HelloCoffee/Services/WebService.swift
+++ HelloCoffee/HelloCoffee/Services/WebService.swift
@@ -31,6 +31,19 @@ class WebService {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = try JSONEncoder().encode(order)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+            httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let newOrder = try? JSONDecoder().decode(Order.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return newOrder
     }
     
     func getOrders() async throws -> [Order] {
