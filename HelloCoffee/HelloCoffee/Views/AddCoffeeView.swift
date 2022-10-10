034f8436d6474ac346388558d9004fd6535641cb
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-10 15:01:19 -0400

    create add coffee view

---
+++ HelloCoffee/HelloCoffee/Views/AddCoffeeView.swift
@@ -0,0 +1,20 @@
+//
+//  AddCoffeeView.swift
+//  HelloCoffee
+//
+//  Created by Thomas Cowern on 10/10/22.
+//
+
+import SwiftUI
+
+struct AddCoffeeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCoffeeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCoffeeView()
+    }
+}
