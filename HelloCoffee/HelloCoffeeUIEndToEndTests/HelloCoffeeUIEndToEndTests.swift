241d8aef27f774e5a0202f46ee501ad0457ce1d3
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-11 14:21:06 -0400

    update new order test to fix testing issues

--- HelloCoffee/HelloCoffeeUIEndToEndTests/HelloCoffeeUIEndToEndTests.swift
+++ HelloCoffee/HelloCoffeeUIEndToEndTests/HelloCoffeeUIEndToEndTests.swift
@@ -11,35 +11,50 @@ final class when_adding_a_new_coffee_order: XCTestCase {
     
     private var app: XCUIApplication!
     
+    // called before running each test
     override func setUp() {
+        
         app = XCUIApplication()
         continueAfterFailure = false
         app.launchEnvironment = ["ENV": "TEST"]
+        app.launch()
         
+        // go to place order screen
         app.buttons["addNewOrderButton"].tap()
-        
+        // fill out the textfields
         let nameTextField = app.textFields["name"]
         let coffeeNameTextField = app.textFields["coffeeName"]
         let priceTextField = app.textFields["price"]
-        let placeOrderButton = app.textFields["placeOrderButton"]
+        let placeOrderButton = app.buttons["placeOrderButton"]
         
         nameTextField.tap()
-        nameTextField.typeText("Bill")
+        nameTextField.typeText("John")
         
         coffeeNameTextField.tap()
         coffeeNameTextField.typeText("Hot Coffee")
         
         priceTextField.tap()
-        priceTextField.typeText("2.50")
+        priceTextField.typeText("4.50")
         
+        // place the order
         placeOrderButton.tap()
     }
     
     func test_should_display_coffee_order_in_list_successfully() throws {
-        XCTAssertEqual("Bill", app.staticTexts["orderNameText"].label)
-        XCTAssertEqual("Hot Coffee", app.staticTexts["coffeeNameAndSizeText"].label)
-        XCTAssertEqual("$2.50", app.textFields["coffeePriceText"].label)
+        
+        XCTAssertEqual("John", app.staticTexts["orderNameText"].label)
+        XCTAssertEqual("Hot Coffee (Medium)", app.staticTexts["coffeeNameAndSizeText"].label)
+        XCTAssertEqual("$4.50", app.staticTexts["coffeePriceText"].label)
     }
+    
+    // called after running each test
+    override func tearDown() {
+        Task {
+            guard let url = URL(string: "/test/clear-orders", relativeTo: URL(string: "https://island-bramble.glitch.me")!) else { return }
+            let (_, _) = try! await URLSession.shared.data(from: url)
+        }
+    }
+    
 }
 
 
