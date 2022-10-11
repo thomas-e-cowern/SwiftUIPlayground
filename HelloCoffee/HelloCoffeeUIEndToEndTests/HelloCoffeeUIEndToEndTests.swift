1189d49b061f6096b8ee2a3b98a096fece7bcabb
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-11 14:01:34 -0400

    add test case for new coffee order to end to end tests

--- HelloCoffee/HelloCoffeeUIEndToEndTests/HelloCoffeeUIEndToEndTests.swift
+++ HelloCoffee/HelloCoffeeUIEndToEndTests/HelloCoffeeUIEndToEndTests.swift
@@ -7,6 +7,20 @@
 
 import XCTest
 
+final class when_adding_a_new_coffee_order: XCTestCase {
+    
+    private var app: XCUIApplication
+    
+    override func setUp() async throws {
+        app = XCUIApplication()
+        continueAfterFailure = false
+        app.launchEnvironment = ["ENV": "TEST"]
+        
+        app.buttons["addNewOrderButton"].tap()
+    }
+}
+
+
 final class HelloCoffeeUIEndToEndTests: XCTestCase {
 
     func test_should_make_sure_no_orders_displayed() throws {
