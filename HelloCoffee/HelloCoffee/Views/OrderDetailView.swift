0da1e21bb58b30312b292c8bd4f6840702fbb2a5
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 14:09:27 -0400

    update OrderDetailView with environment variables

--- HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
+++ HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
@@ -16,6 +16,7 @@ struct EditCoffeeErrors {
 struct OrderDetailView: View {
     
     let orderId: Int
+    @EnvironmentObject private var model: CoffeeModel
     @State private var name: String = ""
     @State private var coffeeName: String = ""
     @State private var price: String = ""
@@ -24,50 +25,56 @@ struct OrderDetailView: View {
     
     var body: some View {
         
-        Form {
-            TextField("Name", text: $name)
-                .accessibilityIdentifier("name")
-            Text(errors.name)
-                .visible(!errors.name.isEmpty)
-                .font(.caption)
-            
-            TextField("Coffee Name", text: $coffeeName)
-                .accessibilityIdentifier("coffeeName")
-            Text(errors.coffeeName)
-                .visible(!errors.coffeeName.isEmpty)
-                .font(.caption)
-            
-            TextField("Price", text: $price)
-                .accessibilityIdentifier("price")
-            Text(errors.price)
-                .visible(!errors.price.isEmpty)
-                .font(.caption)
-            
-            Picker("Select Size", selection: $coffeeSize) {
-                ForEach(CoffeeSize.allCases, id: \.rawValue) { size in
-                    Text(size.rawValue).tag(size)
-                }
-            }
-            .pickerStyle(.segmented)
-            
-            Button("Place Order") {
+        if let order = model.getOrderById(orderId) {
+            Form {
+                TextField("Name", text: $name)
+                    .accessibilityIdentifier("name")
+                Text(errors.name)
+                    .visible(!errors.name.isEmpty)
+                    .font(.caption)
                 
-                if isValid {
-                    Task {
-                        await placeOrder()
+                TextField("Coffee Name", text: $coffeeName)
+                    .accessibilityIdentifier("coffeeName")
+                Text(errors.coffeeName)
+                    .visible(!errors.coffeeName.isEmpty)
+                    .font(.caption)
+                
+                TextField("Price", text: $price)
+                    .accessibilityIdentifier("price")
+                Text(errors.price)
+                    .visible(!errors.price.isEmpty)
+                    .font(.caption)
+                
+                Picker("Select Size", selection: $coffeeSize) {
+                    ForEach(CoffeeSize.allCases, id: \.rawValue) { size in
+                        Text(size.rawValue).tag(size)
                     }
                 }
+                .pickerStyle(.segmented)
                 
+                Button("Place Order") {
+                    
+//                    if isValid {
+//                        Task {
+//                            await placeOrder()
+//                        }
+//                    }
+                    
+                }
+                .accessibilityIdentifier("placeOrderButton")
+                .centerHorizontally()
             }
-            .accessibilityIdentifier("placeOrderButton")
-            .centerHorizontally()
+            .navigationTitle("Add a Coffee")
         }
-        .navigationTitle("Add a Coffee")
     }
 }
 
 struct OrderDetailView_Previews: PreviewProvider {
     static var previews: some View {
+        
+        var config = Configuration()
+        
         OrderDetailView(orderId: 1)
+            .environmentObject(CoffeeModel(webService: WebService(baseUrl: config.environment.baseUrl)))
     }
 }
