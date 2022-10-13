12de8bca50aac7ce933e31f73a2cb846e5f28e4a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 14:25:38 -0400

    add isPresented variable and sheet to OrderDetailVie

--- HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
+++ HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
@@ -17,11 +17,13 @@ struct OrderDetailView: View {
     
     let orderId: Int
     @EnvironmentObject private var model: CoffeeModel
-    @State private var name: String = ""
-    @State private var coffeeName: String = ""
-    @State private var price: String = ""
-    @State private var coffeeSize: CoffeeSize = .medium
-    @State private var errors: EditCoffeeErrors = EditCoffeeErrors()
+    @State private var isPresented: Bool = false
+    
+//    @State private var name: String = ""
+//    @State private var coffeeName: String = ""
+//    @State private var price: String = ""
+//    @State private var coffeeSize: CoffeeSize = .medium
+//    @State private var errors: EditCoffeeErrors = EditCoffeeErrors()
     
     var body: some View {
         
@@ -41,49 +43,14 @@ struct OrderDetailView: View {
                 .accessibilityIdentifier("deleteOrderButton")
                 Button("Edit Order") {
                     // Edit here
+                    isPresented = true
                 }
                 .accessibilityIdentifier("editOrderButton")
                 Spacer()
             }
-//            Form {
-//                TextField("Name", text: $name)
-//                    .accessibilityIdentifier("name")
-//                Text(errors.name)
-//                    .visible(!errors.name.isEmpty)
-//                    .font(.caption)
-//
-//                TextField("Coffee Name", text: $coffeeName)
-//                    .accessibilityIdentifier("coffeeName")
-//                Text(errors.coffeeName)
-//                    .visible(!errors.coffeeName.isEmpty)
-//                    .font(.caption)
-//
-//                TextField("Price", text: $price)
-//                    .accessibilityIdentifier("price")
-//                Text(errors.price)
-//                    .visible(!errors.price.isEmpty)
-//                    .font(.caption)
-//
-//                Picker("Select Size", selection: $coffeeSize) {
-//                    ForEach(CoffeeSize.allCases, id: \.rawValue) { size in
-//                        Text(size.rawValue).tag(size)
-//                    }
-//                }
-//                .pickerStyle(.segmented)
-//
-//                Button("Place Order") {
-//
-//                    if isValid {
-//                        Task {
-//                            await placeOrder()
-//                        }
-//                    }
-//
-//                }
-//                .accessibilityIdentifier("placeOrderButton")
-//                .centerHorizontally()
-//            }
-//            .navigationTitle("Add a Coffee")
+            .sheet(isPresented: $isPresented) {
+                AddCoffeeView()
+            }
         }
     }
 }
