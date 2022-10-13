dba68e5f22f2cd3d7b9287c0b3afe53bc7e409ec
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 15:16:55 -0400

    update sheet call in OrderDetailView

--- HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
+++ HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
@@ -49,7 +49,7 @@ struct OrderDetailView: View {
                 Spacer()
             }
             .sheet(isPresented: $isPresented) {
-                AddCoffeeView()
+                AddCoffeeView(order: order)
             }
         }
     }
