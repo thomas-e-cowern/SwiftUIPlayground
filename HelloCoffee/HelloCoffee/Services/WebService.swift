be002f281f2982d90643b53ff50e07a8fd30495c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 12:06:52 -0400

    update updateOrder in webservice

--- HelloCoffee/HelloCoffee/Services/WebService.swift
+++ HelloCoffee/HelloCoffee/Services/WebService.swift
@@ -98,6 +98,30 @@ class WebService {
         guard let orderId = order.id else {
             throw NetworkError.badId
         }
+        
+        guard let url = URL(string: Endpoints.updateOrder(orderId).path, relativeTo: baseUrl) else {
+            print("Bad url")
+            throw NetworkError.badUrl
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try JSONEncoder().encode(order)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+            httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let updatedOrder = try? JSONDecoder().decode(Order.self, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return updatedOrder
+
     }
     
 }
