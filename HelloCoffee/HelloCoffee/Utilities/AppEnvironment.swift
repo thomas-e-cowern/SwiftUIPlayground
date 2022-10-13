901e5968865059e60fb132f5550e76c3ae2d3552
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 12:00:48 -0400

    update endpoints with updateOrder path

--- HelloCoffee/HelloCoffee/Utilities/AppEnvironment.swift
+++ HelloCoffee/HelloCoffee/Utilities/AppEnvironment.swift
@@ -11,6 +11,7 @@ enum Endpoints {
     case allOrders
     case placeOrder
     case deleteOrder(Int)
+    case updateOrder(Int)
     
     var path: String {
         switch self {
@@ -19,6 +20,8 @@ enum Endpoints {
         case .placeOrder:
             return "/test/new-order"
         case .deleteOrder(let orderId):
+            return "/test/orders/\(orderId)"
+        case .updateOrder(let orderId):
             return "/test/orders/\(orderId)"
         }
     }
