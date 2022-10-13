45dded2b248ddfb4f231f9815ea1dce83baa19c3
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-13 13:36:49 -0400

    add orderDetailView to views group

---
+++ HelloCoffee/HelloCoffee/Views/OrderDetailView.swift
@@ -0,0 +1,23 @@
+//
+//  OrderDetailView.swift
+//  HelloCoffee
+//
+//  Created by Thomas Cowern on 10/13/22.
+//
+
+import SwiftUI
+
+struct OrderDetailView: View {
+    
+    let orderId: Int
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetailView(orderId: 1)
+    }
+}
