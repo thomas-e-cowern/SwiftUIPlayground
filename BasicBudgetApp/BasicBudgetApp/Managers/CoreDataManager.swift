e52bb1cc3c66ceb6c8c94863217a7ad3204ef5d7
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-17 13:50:56 -0400

    update CoreDataManager with singleton, persistentContainer, and initializer

--- BasicBudgetApp/BasicBudgetApp/Managers/CoreDataManager.swift
+++ BasicBudgetApp/BasicBudgetApp/Managers/CoreDataManager.swift
@@ -7,3 +7,23 @@
 
 import Foundation
 import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    private var persistentContainer: NSPersistentContainer
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "BudgetModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error {
+                    fatalError("Unable to initialize Core Data Stack; \(error)")
+            }
+        }
+    }
+    
+    var viewContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+}
