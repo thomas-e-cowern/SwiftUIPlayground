1e475e39c7ffe07a109caf23fad06522a45560dc
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-17 15:42:57 -0400

    add environmet varialbe, FetchRequest, and List to content view

--- BasicBudgetApp/BasicBudgetApp/ContentView.swift
+++ BasicBudgetApp/BasicBudgetApp/ContentView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: []) private var budgetCategoryResults: FetchedResults<BudgetCategory>
     @State private var isPresented: Bool = false
     
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                List(budgetCategoryResults) { budgetCategory in
+                    Text(budgetCategory.title ?? "")
+                }
             }
             .padding()
             .toolbar {
