3565b1e79b88da44c60bf8e056022081eafaf9c9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-17 13:26:43 -0400

    add BasicBudgetApp project to SwiftUIPlayground

---
+++ BasicBudgetApp/BasicBudgetApp/BasicBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicBudgetAppApp.swift
+//  BasicBudgetApp
+//
+//  Created by Thomas Cowern on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
