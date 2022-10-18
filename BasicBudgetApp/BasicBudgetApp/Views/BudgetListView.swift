96ca396dad8cd623dccba859e01bc9acd40eee0d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-18 14:27:06 -0400

    add BudgetListView

---
+++ BasicBudgetApp/BasicBudgetApp/Views/BudgetListView.swift
@@ -0,0 +1,20 @@
+//
+//  BudgetListView.swift
+//  BasicBudgetApp
+//
+//  Created by Thomas Cowern on 10/18/22.
+//
+
+import SwiftUI
+
+struct BudgetListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BudgetListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BudgetListView()
+    }
+}
