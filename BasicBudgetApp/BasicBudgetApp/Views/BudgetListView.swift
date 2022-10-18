bb7cf8433062ef542cbd63b78d686e9b064b8b3b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-18 14:29:42 -0400

    update BudgetListView with fetchedResulst

--- BasicBudgetApp/BasicBudgetApp/Views/BudgetListView.swift
+++ BasicBudgetApp/BasicBudgetApp/Views/BudgetListView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct BudgetListView: View {
+    
+    let BudgetCategoryResults: FetchedResults<BudgetCategory>
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct BudgetListView_Previews: PreviewProvider {
-    static var previews: some View {
-        BudgetListView()
-    }
-}
+//struct BudgetListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BudgetListView(BudgetCategoryResults: <#FetchedResults<BudgetCategory>#>)
+//    }
+//}
