3a5e039d21910c9077dd9fd02c16682265438bf6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-19 13:54:38 -0400

    add TransactionListView

---
+++ BasicBudgetApp/BasicBudgetApp/Views/TransactionListView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionListView.swift
+//  BasicBudgetApp
+//
+//  Created by Thomas Cowern on 10/19/22.
+//
+
+import SwiftUI
+
+struct TransactionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionListView()
+    }
+}
