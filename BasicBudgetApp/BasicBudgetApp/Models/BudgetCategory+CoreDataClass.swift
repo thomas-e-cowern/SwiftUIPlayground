4f35402a2b818d5fd8c9727be43ec25de016dba2
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-20 16:01:04 -0400

    add BudgetCategory+CoreDataManager

--- BasicBudgetApp/BasicBudgetApp/Models/BudgetCategory+CoreDataClass.swift
+++ BasicBudgetApp/BasicBudgetApp/Models/BudgetCategory+CoreDataClass.swift
@@ -37,6 +37,14 @@ public class BudgetCategory: NSManagedObject {
         }
     }
     
+    static func byId(_ id: NSManagedObjectID) -> BudgetCategory {
+        let vc = CoreDataManager.shared.viewContext
+        guard let budgetCategory = vc.object(with: id) as? BudgetCategory else {
+            fatalError("Id not found for BudgetCategory")
+        }
+        return budgetCategory
+    }
+    
     static func transactionsByCategoryRequest(_ budgetCategory: BudgetCategory) -> NSFetchRequest<Transaction> {
         let request = Transaction.fetchRequest()
         request.sortDescriptors = [NSSortDescriptor(key: "dateCreated", ascending: false)]
