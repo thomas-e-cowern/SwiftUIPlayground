44dc9ffa5d5193b500fada772c1766eae65156c8
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-20 11:18:45 -0400

    update TransactionListView with onDeleteTransaction closure

--- BasicBudgetApp/BasicBudgetApp/Views/TransactionListView.swift
+++ BasicBudgetApp/BasicBudgetApp/Views/TransactionListView.swift
@@ -11,9 +11,11 @@ import CoreData
 struct TransactionListView: View {
     
     @FetchRequest var transactions: FetchedResults<Transaction>
+    let onDeleteTransaction: (Transaction) -> Void
         
-    init(request: NSFetchRequest<Transaction>) {
+    init(request: NSFetchRequest<Transaction>, onDeleteTransaction: @escaping (Transaction) -> Void) {
         _transactions = FetchRequest(fetchRequest: request)
+        self.onDeleteTransaction = onDeleteTransaction
     }
     
     var body: some View {
@@ -27,6 +29,9 @@ struct TransactionListView: View {
                         Spacer()
                         Text(transaction.total as NSNumber, formatter: NumberFormatter.currency)
                     }
+                }
+                .onDelete { offsets in
+                    offsets.map { transactions[$0] }.forEach(onDeleteTransaction)
                 }
             }
         }
