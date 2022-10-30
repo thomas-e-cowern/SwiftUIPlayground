98de8f526379d8a600e34dd47e2a46e3364e8c40
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-30 17:19:18 -0400

    add ContentViewModel

---
+++ SwiftUICombine/SwiftUICombine/ViewModels/ContentViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ContentViewModel.swift
+//  SwiftUICombine
+//
+//  Created by Thomas Cowern on 10/30/22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ContentViewModel: ObservableObject {
+    init() {
+        
+    }
+}
