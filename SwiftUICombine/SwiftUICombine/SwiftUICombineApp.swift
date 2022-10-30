5f3a50ea9afa346d8bc7956b9133c52c88127286
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-10-30 17:07:19 -0400

    SwiftUICombineApp initial commit

---
+++ SwiftUICombine/SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Thomas Cowern on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
