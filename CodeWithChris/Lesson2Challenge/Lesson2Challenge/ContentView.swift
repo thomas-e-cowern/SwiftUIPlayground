96f037780b21735d26eefa3214ab40898e68e399
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 17:04:46 -0400

    finished Lesson2Challenge

--- CodeWithChris/Lesson2Challenge/Lesson2Challenge/ContentView.swift
+++ CodeWithChris/Lesson2Challenge/Lesson2Challenge/ContentView.swift
@@ -10,10 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke()
+                    .stroke(style: StrokeStyle(lineWidth: 3))
+                    .foregroundColor(Color.blue)
+                    .frame(width: 200, height: 200)
+                
+                Circle()
+                    .stroke()
+                    .frame(width: 190)
+                
+                Ellipse()
+                    .stroke()
+                    .stroke(style: StrokeStyle(dash: [8, 10]))
+                    .frame(width: 85, height: 190)
+                
+                Ellipse()
+                    .stroke()
+                    .stroke(style: StrokeStyle(dash: [8, 10]))
+                    .frame(width: 190, height: 85)
+                
+                ZStack {
+                    Circle()
+                        .foregroundColor(.red)
+                        .frame(width: 75)
+                    Circle()
+                        .foregroundColor(.orange)
+                        .frame(width: 60)
+                    Circle()
+                        .foregroundColor(.yellow)
+                        .frame(width: 45)
+                    Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 30)
+                }
+                
+            }
         }
         .padding()
     }
