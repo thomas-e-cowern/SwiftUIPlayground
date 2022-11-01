d2feaa26b41c1bd356d81bc4f4b0d813edf2a83f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 17:09:04 -0400

    PathLesson initial commit

---
+++ CodeWithChris/PathLesson/PathLesson/PathLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathLessonApp.swift
+//  PathLesson
+//
+//  Created by Thomas Cowern on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct PathLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
