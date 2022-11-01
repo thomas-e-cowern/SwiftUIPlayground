7ed7c76feebab8897023cf8a40c1b17d09c89b91
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 16:34:37 -0400

    initial commit ShapesDemo

---
+++ ShapesDemo/ShapesDemo/ShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesDemoApp.swift
+//  ShapesDemo
+//
+//  Created by Thomas Cowern on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
