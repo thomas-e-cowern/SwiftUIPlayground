eaadccbb707b218adaa108adeb5df6a5bac7f47d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 16:54:43 -0400

    add Lesson2Challenge to CodeWithChris

---
+++ CodeWithChris/Lesson2Challenge/Lesson2Challenge/Lesson2ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2ChallengeApp.swift
+//  Lesson2Challenge
+//
+//  Created by Thomas Cowern on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson2ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
