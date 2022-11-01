737a98988860fa4cd0bdb2e55366f738000ecb29
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 17:24:45 -0400

    add customShape computed variable to ContentView

--- CodeWithChris/PathLesson/PathLesson/ContentView.swift
+++ CodeWithChris/PathLesson/PathLesson/ContentView.swift
@@ -8,29 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let customShape: Path = {
+        return Path({ path in
+            // Commands to draw path
+            path.move(to: CGPoint(x: 100, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 200))
+            path.addLine(to: CGPoint(x: 200, y: 200))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 100))
+            path.closeSubpath()
+            
+            path.move(to: CGPoint(x: 300, y: 300))
+            path.addLines([
+                CGPoint(x: 200, y: 400),
+                CGPoint(x: 300, y: 400),
+                CGPoint(x: 300, y: 300),
+                CGPoint(x: 200, y: 300)
+            ])
+            path.closeSubpath()
+        })
+    }()
+    
     var body: some View {
         VStack {
-            Path({ path in
-                // Commands to draw path
-                path.move(to: CGPoint(x: 100, y: 100))
-                path.addLine(to: CGPoint(x: 100, y: 200))
-                path.addLine(to: CGPoint(x: 200, y: 200))
-                path.addLine(to: CGPoint(x: 200, y: 100))
-                path.addLine(to: CGPoint(x: 100, y: 100))
-                path.closeSubpath()
-                
-                path.move(to: CGPoint(x: 300, y: 300))
-                path.addLines([
-                    CGPoint(x: 200, y: 400),
-                    CGPoint(x: 300, y: 400),
-                    CGPoint(x: 300, y: 300),
-                    CGPoint(x: 200, y: 300)
-                ])
-                path.closeSubpath()
-                
-            })
-//            .stroke(style: StrokeStyle(lineWidth: 2))
+            customShape
             .foregroundColor(.red)
+            .rotationEffect(Angle(degrees: 25))
+            .scaleEffect(0.7)
+            
+            customShape
         
         }
     }
