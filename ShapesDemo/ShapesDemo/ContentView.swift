7695b1f969ad730623a46700e09c6e97f0a15c0b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 16:46:25 -0400

    add Elipse and Rectanble to ContentView

--- ShapesDemo/ShapesDemo/ContentView.swift
+++ ShapesDemo/ShapesDemo/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Circle()
-                .stroke(Color.black, style: StrokeStyle(lineWidth: 10, dash: [1, 1], dashPhase: 7))
-                .frame(width: 100)
-            Circle()
-                .foregroundColor(Color.yellow)
-                .frame(width: 100)
+        VStack {
+            ZStack {
+                Circle()
+                    .stroke(Color.black, style: StrokeStyle(lineWidth: 10, dash: [1, 1], dashPhase: 7))
+                    .frame(width: 100)
+                Circle()
+                    .foregroundColor(Color.yellow)
+                    .frame(width: 100)
+            }
+            
+            Ellipse()
+                .frame(width: 100, height: 50)
+            
+            Rectangle()
+                .foregroundColor(Color.blue)
+                .frame(width: 100, height: 100)
         }
     }
 }
