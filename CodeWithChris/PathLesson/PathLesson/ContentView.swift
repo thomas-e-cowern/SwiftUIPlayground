438199f20c7344f1d88b219cb26e8b71284b4ec4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-01 17:20:16 -0400

    add Path to draw 2 squares to ContentView

--- CodeWithChris/PathLesson/PathLesson/ContentView.swift
+++ CodeWithChris/PathLesson/PathLesson/ContentView.swift
@@ -10,12 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Path({ path in
+                // Commands to draw path
+                path.move(to: CGPoint(x: 100, y: 100))
+                path.addLine(to: CGPoint(x: 100, y: 200))
+                path.addLine(to: CGPoint(x: 200, y: 200))
+                path.addLine(to: CGPoint(x: 200, y: 100))
+                path.addLine(to: CGPoint(x: 100, y: 100))
+                path.closeSubpath()
+                
+                path.move(to: CGPoint(x: 300, y: 300))
+                path.addLines([
+                    CGPoint(x: 200, y: 400),
+                    CGPoint(x: 300, y: 400),
+                    CGPoint(x: 300, y: 300),
+                    CGPoint(x: 200, y: 300)
+                ])
+                path.closeSubpath()
+                
+            })
+//            .stroke(style: StrokeStyle(lineWidth: 2))
+            .foregroundColor(.red)
+        
         }
-        .padding()
     }
 }
 
