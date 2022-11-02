55b120e0467aab42fc2a3d4445f208378e7a6f84
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 13:08:05 -0400

    add LoginRequestBody struct to WebService

--- SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
+++ SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
@@ -12,9 +12,21 @@ enum AuthenticationError: Error {
     case custom(errorMessage: String)
 }
 
+struct LoginRequestBody: Codable {
+    let username: String
+    let password: String
+}
+
 class WebService {
     
-    func login(username: String, password: String, completion: @escaping Result<String, AuthenticationError>) -> Void {
+    func login(username: String, password: String, completion: @escaping (Result<String, AuthenticationError>) -> Void) {
+        
+        guard let url = URL(string: "https://four-harvest-hovercraft.glitch.me/login") else {
+            completion(.failure(.custom(errorMessage: "URL is not correct")))
+            return
+        }
+        
+        
         
     }
     
