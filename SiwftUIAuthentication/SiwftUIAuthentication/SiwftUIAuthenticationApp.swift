61d3b02b5c9eee3025add8515621bdb7bda5a38f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 12:53:17 -0400

    add SwiftUIAuthentication project

---
+++ SiwftUIAuthentication/SiwftUIAuthentication/SiwftUIAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiwftUIAuthenticationApp.swift
+//  SiwftUIAuthentication
+//
+//  Created by Thomas Cowern on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SiwftUIAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
