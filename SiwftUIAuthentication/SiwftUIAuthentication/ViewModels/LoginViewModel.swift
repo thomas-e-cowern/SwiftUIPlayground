9110be1aa894aa0bdeab964e3a25d13d485a8db4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 14:40:45 -0400

    add ViewModels folder and LoginViewModel

---
+++ SiwftUIAuthentication/SiwftUIAuthentication/ViewModels/LoginViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModel.swift
+//  SiwftUIAuthentication
+//
+//  Created by Thomas Cowern on 11/2/22.
+//
+
+import Foundation
+ 
+struct LoginViewModel: ObservableObject {
+    
+}
