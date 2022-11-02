45a0141d299cfd11b7c454a42b49cfed3b70d4f3
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 13:13:39 -0400

    add request to WebService

--- SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
+++ SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
@@ -26,7 +26,21 @@ class WebService {
             return
         }
         
+        let body = LoginRequestBody(username: username, password: password)
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(.failure(.custom(errorMessage: "Bad Data")))
+                return
+            }
+            
+            
+        }.resume()
         
     }
     
