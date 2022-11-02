b81e5fb3a6b4b17467eb563fa346fa01d03a87b2
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 13:03:35 -0400

    add AuthenticationError enum to WebService

--- SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
+++ SiwftUIAuthentication/SiwftUIAuthentication/Services/WebService.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+enum AuthenticationError: Error {
+    case invalidCredentials
+    case custom(errorMessage: String)
+}
+
+class WebService {
+    
+    func login(username: String, password: String, completion: @escaping Result<String, AuthenticationError>) -> Void {
+        
+    }
+    
+}
