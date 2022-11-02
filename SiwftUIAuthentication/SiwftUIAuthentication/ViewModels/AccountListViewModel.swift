70e215fe3221bcd74b40d5696bfaa41f6745a067
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 17:35:32 -0400

    update AccountViewListModel getAllAccounts method

--- SiwftUIAuthentication/SiwftUIAuthentication/ViewModels/AccountListViewModel.swift
+++ SiwftUIAuthentication/SiwftUIAuthentication/ViewModels/AccountListViewModel.swift
@@ -9,4 +9,27 @@ import Foundation
 
 class AccountViewListModel: ObservableObject {
     
+    @Published var accounts: [AccountViewModel] = []
+    
+    func getAllAccounts() {
+        
+        let defaults = UserDefaults.standard
+        
+        guard let token = defaults.string(forKey: "jsonwebtoken") else {
+            return
+        }
+        
+        WebService().getAllAccounts(token: token) { result in
+            switch result {
+            case .success(let accounts):
+                print(accounts)
+                DispatchQueue.main.async {
+                    self.accounts = accounts.map(AccountViewModel.init)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
 }
