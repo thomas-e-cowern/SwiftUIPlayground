959480b5426ab78351cb31692640250a7b12b374
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 12:58:28 -0400

    add Image and Buttons to ContentView

--- SiwftUIAuthentication/SiwftUIAuthentication/ContentView.swift
+++ SiwftUIAuthentication/SiwftUIAuthentication/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+                HStack {
+                    Spacer()
+                    Image(systemName: "lock.fill")
+                }
+                
+                HStack {
+                    Spacer()
+                    Button("Login") {
+                        
+                    }
+                    Button("Logoout") {
+                        
+                    }
+                    Spacer()
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
         }
-        .padding()
     }
 }
 
