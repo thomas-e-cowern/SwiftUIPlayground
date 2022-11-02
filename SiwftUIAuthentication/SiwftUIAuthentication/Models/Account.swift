3b06ec55f778aa550fc47b70d8a2799071e1588e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 16:46:57 -0400

    add Models folder and Account file

---
+++ SiwftUIAuthentication/SiwftUIAuthentication/Models/Account.swift
@@ -0,0 +1,13 @@
+//
+//  Account.swift
+//  SiwftUIAuthentication
+//
+//  Created by Thomas Cowern on 11/2/22.
+//
+
+import Foundation
+
+struct Account: Decodable {
+    let name: String
+    let balance: Double
+}
