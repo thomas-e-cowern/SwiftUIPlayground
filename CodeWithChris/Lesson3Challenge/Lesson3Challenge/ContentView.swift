2f7e6033fe00ccc6f96a8e90f155d0a8a1868d29
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 09:57:45 -0400

    add triangle Path to ContentVIew

--- CodeWithChris/Lesson3Challenge/Lesson3Challenge/ContentView.swift
+++ CodeWithChris/Lesson3Challenge/Lesson3Challenge/ContentView.swift
@@ -27,10 +27,14 @@ struct ContentView: View {
             .foregroundColor(.blue)
             
             Path({ path in
-                path.move(to: CGPoint(x: <#T##Double#>, y: <#T##Double#>))
+                path.move(to: CGPoint(x: 70, y: 350))
+                path.addLine(to: CGPoint(x: 100, y: 350))
+                path.addLine(to: CGPoint(x: 82, y: 400))
+                path.addLine(to: CGPoint(x: 70, y: 350))
+                path.closeSubpath()
             })
+            .foregroundColor(.yellow)
         }
-        .padding()
     }
 }
 
