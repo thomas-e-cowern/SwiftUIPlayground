bfed3426534ae707cd40917f38aaf295ff955239
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-02 09:31:12 -0400

    add Lesson3Challenge

---
+++ CodeWithChris/Lesson3Challenge/Lesson3Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lesson3Challenge
+//
+//  Created by Thomas Cowern on 11/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CodeWithChris/Lesson3Challenge/Lesson3Challenge/Lesson3ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson3ChallengeApp.swift
+//  Lesson3Challenge
+//
+//  Created by Thomas Cowern on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson3ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
