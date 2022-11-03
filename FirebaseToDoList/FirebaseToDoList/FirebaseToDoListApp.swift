3097f11f02b02b4212578c78f02b42a3109eb1a1
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-03 15:43:51 -0400

    add FirebaseToDoList to SwiftUIPlayground

---
+++ FirebaseToDoList/FirebaseToDoList/FirebaseToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseToDoListApp.swift
+//  FirebaseToDoList
+//
+//  Created by Thomas Cowern on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
