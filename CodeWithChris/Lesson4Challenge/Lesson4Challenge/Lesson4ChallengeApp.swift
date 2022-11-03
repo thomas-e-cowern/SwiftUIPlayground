2c793edc3a47908d221e88ebba8677d5ce51e8e0
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-03 12:39:26 -0400

    add Lesson4Challenge to CodeWithChris

---
+++ CodeWithChris/Lesson4Challenge/Lesson4Challenge/Lesson4ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4ChallengeApp.swift
+//  Lesson4Challenge
+//
+//  Created by Thomas Cowern on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
