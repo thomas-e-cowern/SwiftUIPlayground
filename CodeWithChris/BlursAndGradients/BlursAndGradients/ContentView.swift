e10631e358ea0f33003fc4b3d32ad00def3c8ba9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-03 12:25:18 -0400

    add LinearGradient to ContentView

--- CodeWithChris/BlursAndGradients/BlursAndGradients/ContentView.swift
+++ CodeWithChris/BlursAndGradients/BlursAndGradients/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea()
+            
             Capsule()
                 .foregroundColor(.blue)
                 .frame(width: 200, height: 40)
             Text("Howdy")
                 .foregroundColor(.white)
         }
-        .blur(radius: 3, opaque: true)
     }
 }
 
