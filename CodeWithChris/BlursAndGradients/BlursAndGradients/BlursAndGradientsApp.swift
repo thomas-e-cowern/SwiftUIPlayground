45d442c62ac2883f53e9f01cbe76df25fd761df8
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-03 12:16:38 -0400

    add BlursAndGradients to CodeWithChris

---
+++ CodeWithChris/BlursAndGradients/BlursAndGradients/BlursAndGradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlursAndGradientsApp.swift
+//  BlursAndGradients
+//
+//  Created by Thomas Cowern on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BlursAndGradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
