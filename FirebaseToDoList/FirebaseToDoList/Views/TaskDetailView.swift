70c27c4ad4c85c8a5e4aecc22e1399f0b7231567
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-04 18:46:17 -0400

    update TaskDetailView method updateTask to access db and make updates

--- FirebaseToDoList/FirebaseToDoList/Views/TaskDetailView.swift
+++ FirebaseToDoList/FirebaseToDoList/Views/TaskDetailView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct TaskDetailView: View {
     
+    let db: Firestore = Firestore.firestore()
     let task: Task
+    
     @State private var title: String = ""
     
     var body: some View {
@@ -19,15 +23,30 @@ struct TaskDetailView: View {
                 .border(.black)
                 .padding()
             Button("Update") {
-                
+                updateTask()
             }
             .buttonStyle(.bordered)
         }
+    }
+    
+    private func updateTask() {
+        db.collection("tasks")
+            .document(task.id!)
+            .updateData([
+                "title": title
+            ]) { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Document has been updated")
+                }
+            }
+        
     }
 }
 
 struct TaskDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TaskDetailView(task: Task(title: "Test Task"))
+        TaskDetailView(task: Task(id: "123", title: "Test Task"))
     }
 }
