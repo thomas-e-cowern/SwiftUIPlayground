da10d8f7ee02b45b66eaa8d3909a4dfd14daff02
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-04 18:25:39 -0400

    add Views folder and TaskDetailView file

---
+++ FirebaseToDoList/FirebaseToDoList/Views/TaskDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskDetailView.swift
+//  FirebaseToDoList
+//
+//  Created by Thomas Cowern on 11/4/22.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskDetailView()
+    }
+}
