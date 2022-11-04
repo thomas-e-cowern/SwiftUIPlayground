bd625f823716e141077b7d6cb9a3f9ec66b18042
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-04 18:27:26 -0400

    add new listStyle to List in ContentView

--- FirebaseToDoList/FirebaseToDoList/ContentView.swift
+++ FirebaseToDoList/FirebaseToDoList/ContentView.swift
@@ -44,6 +44,7 @@ struct ContentView: View {
                     }
                     .onDelete(perform: deleteTask)
                 }
+                .listStyle(.plain)
                 
                 Spacer()
                     .onAppear {
