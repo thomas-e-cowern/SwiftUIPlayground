37ad634e80640a7fbbe13d29b663cee4ede26544
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-08 18:24:53 -0500

    add Store model to Models folder

---
+++ GroceryApp/GroceryApp/Models/Store.swift
@@ -0,0 +1,14 @@
+//
+//  Store.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/8/22.
+//
+
+import Foundation
+
+struct Store: Codable {
+    var id: String?
+    var name: String
+    var address: String
+}
