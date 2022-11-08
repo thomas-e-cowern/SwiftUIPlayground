d7d22107d3f0f455fa6f3112732de058d01553a6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-08 18:21:55 -0500

    add Constants file

---
+++ GroceryApp/GroceryApp/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/8/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Messages {
+        static let storeSavedFailure = "Unable to save the store...."
+    }
+}
