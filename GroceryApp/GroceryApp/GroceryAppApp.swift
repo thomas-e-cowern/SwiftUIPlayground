3a86fb4f3a9fc234340c84d84f032e750212ce88
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-08 18:03:36 -0500

    GroceryApp initial commit

---
+++ GroceryApp/GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
