145d2d97a6f504fe8e68c7508f8e18b9c5c969f6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-08 19:10:45 -0500

    add AddStoreView to project

---
+++ GroceryApp/GroceryApp/Views/AddStoreView.swift
@@ -0,0 +1,33 @@
+//
+//  AddStoreView.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/8/22.
+//
+
+import SwiftUI
+
+struct AddStoreView: View {
+    
+    @StateObject private var addStoreVM = AddStoreViewModel()
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $addStoreVM.name)
+                TextField("Address", text: $addStoreVM.address)
+                HStack {
+                    Button("Save") {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddStoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddStoreView()
+    }
+}
