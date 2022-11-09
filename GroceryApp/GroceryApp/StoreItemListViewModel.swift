fecb327afa0a40a3fa8d63dee99cbe996fb6ee14
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-09 16:37:24 -0500

    add variables, init and addItemsToStore method to StoreItemListViewModel

--- GroceryApp/GroceryApp/StoreItemListViewModel.swift
+++ GroceryApp/GroceryApp/StoreItemListViewModel.swift
@@ -9,6 +9,28 @@ import Foundation
 
 class StoreItemListViewModel: ObservableObject {
     
+    private var firestoreManager: FirestoreManager
     
+    var groceryItemName: String = ""
+    @Published var store: StoreViewModel?
+    
+    init() {
+        firestoreManager = FirestoreManager()
+    }
+    
+    func addItemsToStore(storeId: String) {
+        firestoreManager.updateStore(storeId: storeId, values: ["items": [groceryItemName]]) { result in
+            
+            switch result {
+            case .success(let store):
+                if let model = store {
+                    self.store = StoreViewModel(store: model)
+                }
+            case .failure(let error):
+                print("Error in addItemsToStore: \(error.localizedDescription)")
+            }
+            
+        }
+    }
     
 }
