7de156f39b1d4169c2c40eb3327c42c8c21f3ed7
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-09 16:31:01 -0500

    add StoreItemListVIewModel to ViewModels

---
+++ GroceryApp/GroceryApp/StoreItemListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  StoreItemListViewModel.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/9/22.
+//
+
+import Foundation
+
+class StoreItemListViewModel: ObservableObject {
+    
+    
+    
+}
