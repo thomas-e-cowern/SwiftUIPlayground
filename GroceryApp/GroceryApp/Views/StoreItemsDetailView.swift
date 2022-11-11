a914f1b39e3b0358402fe718b972e07e06553850
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-10 19:18:32 -0500

    add StoreItemDetailView to Views Folder

---
+++ GroceryApp/GroceryApp/Views/StoreItemsDetailView.swift
@@ -0,0 +1,29 @@
+//
+//  StoreItemsDetailView.swift
+//  GroceryApp
+//
+//  Created by Thomas Cowern on 11/10/22.
+//
+
+import SwiftUI
+
+struct StoreItemsDetailView: View {
+    
+    var storeItem: StoreItem
+    
+    var body: some View {
+        VStack {
+            Text(storeItem.name)
+            HStack {
+                Text("\(storeItem.price)")
+                Text("\(storeItem.quantity)")
+            }
+        }
+    }
+}
+
+struct StoreItemsDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreItemsDetailView(storeItem: StoreItem(name: "Test Items", price: 10.0, quantity: 2))
+    }
+}
