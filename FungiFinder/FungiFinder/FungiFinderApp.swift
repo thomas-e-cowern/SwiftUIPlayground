7084e479fe583ac34d3f2cd8d959245670df64d6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-14 11:19:42 -0500

    FungiFinder initial commit

---
+++ FungiFinder/FungiFinder/FungiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FungiFinderApp.swift
+//  FungiFinder
+//
+//  Created by Thomas Cowern on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FungiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
