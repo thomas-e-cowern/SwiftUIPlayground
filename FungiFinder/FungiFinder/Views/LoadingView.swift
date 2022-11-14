a2b2f9db15041da7a5b91d2353df0c5ff3a5b81b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-14 14:13:13 -0500

    add LoadingView to Views

---
+++ FungiFinder/FungiFinder/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  FungiFinder
+//
+//  Created by Thomas Cowern on 11/14/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading View")
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
