74bc44c9270ae634d41dd2a7109181daabc59777
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-15 02:17:11 -0500

    add LoginViewModel to ViewModels

---
+++ FungiFinder/FungiFinder/ViewModels/LoginViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewModel.swift
+//  FungiFinder
+//
+//  Created by Thomas Cowern on 11/15/22.
+//
+
+import Foundation
+import Firebase
+
+class LoginViewModel: ObservableObject {
+    
+    var email: String = ""
+    var password: String = ""
+    
+    func login(completion: @escaping () -> Void) {
+        
+        
+        
+    }
+    
+}
