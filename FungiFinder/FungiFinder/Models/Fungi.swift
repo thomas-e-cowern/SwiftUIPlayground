6129c2f2b34b44d3be0974ddb3cd46a442e677b5
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-15 04:19:51 -0500

    add Fungi model to Models folder

---
+++ FungiFinder/FungiFinder/Models/Fungi.swift
@@ -0,0 +1,15 @@
+//
+//  Fungi.swift
+//  FungiFinder
+//
+//  Created by Thomas Cowern on 11/15/22.
+//
+
+import Foundation
+
+struct Fungi: Codable {
+    var id: String?
+    var name: String = ""
+    var url: String = ""
+    var userId: String = ""
+}
