38eb149ced2454b3779ec37928b3edba049b05d5
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-15 02:03:28 -0500

    add RegisterViewModel to ViewModels

---
+++ FungiFinder/FungiFinder/ViewModels/RegisterViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewModel.swift
+//  FungiFinder
+//
+//  Created by Thomas Cowern on 11/15/22.
+//
+
+import Foundation
+import Firebase
+
+class RegisterViewModel: ObservableObject {
+    
+    var email: String = ""
+    var password: String = ""
+    
+    func register(completion: @escaping () -> Void) {
+        
+    }
+    
+}
