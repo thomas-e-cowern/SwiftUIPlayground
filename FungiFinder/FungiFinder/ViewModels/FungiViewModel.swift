a1d7b48012c88cc91ba5f82eb3d0af450877be29
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-15 11:21:33 -0500

    update FungiViewModel

--- FungiFinder/FungiFinder/ViewModels/FungiViewModel.swift
+++ FungiFinder/FungiFinder/ViewModels/FungiViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct FungiViewModel {
+    let fungi: Fungi
+    
+    var fungiId: String {
+        fungi.id ?? ""
+    }
+    
+    var name: String {
+        fungi.name
+    }
+    
+    var photoUrl: String {
+        fungi.url
+    }
+}
