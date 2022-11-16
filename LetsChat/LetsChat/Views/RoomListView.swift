dc920604bf0879557b22d351ca06902465322e2b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 01:55:40 -0500

    update RoomListView

--- LetsChat/LetsChat/Views/RoomListView.swift
+++ LetsChat/LetsChat/Views/RoomListView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct RoomListView: View {
+    
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("Rooms")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        isPresented = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+
+                }
+            }
+            
+            .sheet(isPresented: $isPresented) {
+                AddRoomView()
+            }
+            
+            .onAppear {
+                // Do somthing...
+            }
+        }
     }
 }
 
