d92250b0d30b2be36833c7e6d87fe39032a82e7a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 01:56:50 -0500

    add AddRoomView

---
+++ LetsChat/LetsChat/Views/AddRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRoomView.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/16/22.
+//
+
+import SwiftUI
+
+struct AddRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRoomView()
+    }
+}
