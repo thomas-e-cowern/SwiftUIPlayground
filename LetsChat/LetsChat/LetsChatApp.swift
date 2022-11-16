f3e2719e2e26b5e72935f8c8ef5b15d4332e4eef
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 01:26:53 -0500

    LetsChat initial commit

---
+++ LetsChat/LetsChat/LetsChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsChatApp.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
