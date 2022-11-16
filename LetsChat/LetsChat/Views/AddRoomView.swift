7b88a457221b0c548d3f7af481f24f32e51ff0f6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 02:15:36 -0500

    update AddRoomView

--- LetsChat/LetsChat/Views/AddRoomView.swift
+++ LetsChat/LetsChat/Views/AddRoomView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct AddRoomView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject private var addRoomVM = AddRoomViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+                Form {
+                    TextField("Enter name", text: $addRoomVM.name)
+                    TextField("Enter description", text: $addRoomVM.description)
+                    
+                }
+                
+                Button("Save") {
+                    addRoomVM.createRoom {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    
+                }.buttonStyle(PrimaryButtonStyle())
+                .padding()
+                
+                Spacer()
+                
+            }.navigationTitle("Add New Room")
+        }
     }
 }
 
