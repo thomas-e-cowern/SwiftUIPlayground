ff3a75bb37f424fc56206af22f3b543c183ceccc
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 04:12:26 -0500

    update AddRoomViewModel with createRoom method

--- LetsChat/LetsChat/ViewModels/AddRoomViewModel.swift
+++ LetsChat/LetsChat/ViewModels/AddRoomViewModel.swift
@@ -6,16 +6,31 @@
 //
 
 import Foundation
-import Firebase
+import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 class AddRoomViewModel: ObservableObject {
     
     var name: String = ""
     var description: String = ""
+    var db = Firestore.firestore()
     
     func createRoom(completion: @escaping() -> Void) {
         
+        let room = Room(name: name, description: description)
+        
+        do {
+          
+          _ = try db.collection("rooms")
+              .addDocument(from: room, encoder: Firestore.Encoder()) { (error) in
+                  if let error = error {
+                      print(error.localizedDescription)
+                  } else {
+                      completion()
+                  }
+              }
+          } catch let error {
+              print(error.localizedDescription)
+          }
     }
-    
 }
