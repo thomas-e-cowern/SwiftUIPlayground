8d75fe2af43d083273bfcb8331e5a187addde05c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 03:54:21 -0500

    add AddRoomViewModel

---
+++ LetsChat/LetsChat/ViewModels/AddRoomViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  AddRoomViewModel.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/16/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class AddRoomViewModel: ObservableObject {
+    
+    var name: String = ""
+    var description: String = ""
+    
+    func createRoom(completion: @escaping() -> Void) {
+        
+    }
+    
+}
