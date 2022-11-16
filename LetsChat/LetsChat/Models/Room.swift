921366582482dc2c1f96231b3ab3fa35910a4617
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 03:56:27 -0500

    add Room model to Models folder

---
+++ LetsChat/LetsChat/Models/Room.swift
@@ -0,0 +1,14 @@
+//
+//  Room.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/16/22.
+//
+
+import Foundation
+
+struct Room: Codable {
+    var id: String?
+    var name: String
+    var description: String
+}
