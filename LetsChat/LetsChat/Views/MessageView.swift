9b949d06582a150084ef0796ca97389408e5f99e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-16 02:20:07 -0500

    add MessageView

---
+++ LetsChat/LetsChat/Views/MessageView.swift
@@ -0,0 +1,25 @@
+//
+//  MessageView.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/16/22.
+//
+
+import SwiftUI
+
+enum MessageStyle {
+    case primary
+    case secondary
+}
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
