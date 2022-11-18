df8e912f233a4b0041371c9ac57d6c07546d88ce
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-18 00:49:43 -0500

    add RoomRowView

---
+++ LetsChat/LetsChat/Views/RoomRowView.swift
@@ -0,0 +1,27 @@
+//
+//  RoomRowView.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/18/22.
+//
+
+import SwiftUI
+
+struct RoomRowView: View {
+    
+    let room: RoomViewModel
+    
+    var body: some View {
+        VStack {
+            Text(room.name)
+                .font(.headline)
+            Text(room.description)
+        }
+    }
+}
+
+struct RoomRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomRowView(room: RoomViewModel(room: Room(name: "test room", description: "test description")))
+    }
+}
