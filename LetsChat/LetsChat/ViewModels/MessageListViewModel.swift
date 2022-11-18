b02aa7e70ed899f943c606a6f85e2310818d5450
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-18 02:39:08 -0500

    add registerUpdatesForRoom to MessageListViewModel

--- LetsChat/LetsChat/ViewModels/MessageListViewModel.swift
+++ LetsChat/LetsChat/ViewModels/MessageListViewModel.swift
@@ -17,7 +17,7 @@ struct MessageViewState {
 
 class MessageListViewModel: ObservableObject {
     
-    @Published var message: [MessageViewModel] = []
+    @Published var messages: [MessageViewModel] = []
     
     let db = Firestore.firestore()
     
@@ -39,6 +39,32 @@ class MessageListViewModel: ObservableObject {
         } catch let error {
             print("Error in do-catch in sendMessage: \(error.localizedDescription)")
         }
+    }
+    
+    func registerUpdatesForRoom(room: RoomViewModel) {
+        
+        db.collection("rooms")
+            .document(room.roomId)
+            .collection("messages")
+            .order(by:  "messageDate", descending: false)
+            .addSnapshotListener { snapshot, error in
+                if let error = error {
+                    print("Error in registerUpdatesForRoom: \(error.localizedDescription)")
+                } else {
+                    if let snapshot = snapshot {
+                        let messages: [MessageViewModel] = snapshot.documents.compactMap { doc in
+                            guard var message: Message = try? doc.data(as: Message.self) else {
+                                return nil
+                            }
+                            message.id = doc.documentID
+                            return MessageViewModel(message: message)
+                        }
+                        
+                        self.messages = messages
+                    }
+                }
+            }
+        
     }
     
 }
