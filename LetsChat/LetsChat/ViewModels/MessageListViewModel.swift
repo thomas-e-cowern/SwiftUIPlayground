a8e48896e4497b5816c69c12c449a678de4148b9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-18 01:55:03 -0500

    add MessageListViewModel to ViewModels

---
+++ LetsChat/LetsChat/ViewModels/MessageListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MessageListViewModel.swift
+//  LetsChat
+//
+//  Created by Thomas Cowern on 11/18/22.
+//
+
+import Foundation
+
+struct MessageViewModel {
+    
+    let message: Message
+    
+    var messageText: String {
+        message.text
+    }
+    
+    var username: String {
+        message.username
+    }
+    
+    var messageId: String {
+        message.id ?? ""
+    }
+    
+}
