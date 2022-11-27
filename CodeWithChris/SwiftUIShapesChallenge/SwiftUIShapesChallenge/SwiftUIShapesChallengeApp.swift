57f51fbf7e8307d3e6e876cdf5a88dc65d25346b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-27 13:59:49 -0500

    add SwiftUISHapedChallenge

---
+++ CodeWithChris/SwiftUIShapesChallenge/SwiftUIShapesChallenge/SwiftUIShapesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapesChallengeApp.swift
+//  SwiftUIShapesChallenge
+//
+//  Created by Thomas Cowern on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapesChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
