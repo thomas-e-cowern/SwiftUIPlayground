312602d5e53ce00a7b66548ffdca1aa11d8b39ed
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-27 14:19:55 -0500

    add CustomTextFieldStyle to project

---
+++ CodeWithChris/SwiftUIShapesChallenge/SwiftUIShapesChallenge/CustomTextFieldStyle.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldStyle.swift
+//  SwiftUIShapesChallenge
+//
+//  Created by Thomas Cowern on 11/27/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    
+    func _body(configuration: TextField<Self.Label>) -> some View {
+        
+    }
+    
+}
