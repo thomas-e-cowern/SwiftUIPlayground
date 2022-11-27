8745878707d328878344a9ffbdc8fbefcc5d47e1
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-27 15:51:26 -0500

    CustomFont initial commit

---
+++ CodeWithChris/CustomFont/CustomFont/CustomFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontApp.swift
+//  CustomFont
+//
+//  Created by Thomas Cowern on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
