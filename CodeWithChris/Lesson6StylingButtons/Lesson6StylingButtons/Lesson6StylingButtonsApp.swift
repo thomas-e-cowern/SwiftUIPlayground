513294c7d7e753be05df62cb6b3024317303f229
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-27 14:42:28 -0500

    add Lesson6StylingButtons

---
+++ CodeWithChris/Lesson6StylingButtons/Lesson6StylingButtons/Lesson6StylingButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson6StylingButtonsApp.swift
+//  Lesson6StylingButtons
+//
+//  Created by Thomas Cowern on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson6StylingButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
