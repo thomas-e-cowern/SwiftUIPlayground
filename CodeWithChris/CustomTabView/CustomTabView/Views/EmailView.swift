993fabdfde29bf6df10b60b69e66fbdccdea5f1f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-30 01:37:01 -0500

    add geometryreader to email view

--- CodeWithChris/CustomTabView/CustomTabView/Views/EmailView.swift
+++ CodeWithChris/CustomTabView/CustomTabView/Views/EmailView.swift
@@ -9,49 +9,51 @@ import SwiftUI
 
 struct EmailView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.green)
-            
-            Image("james-bond-no-time-to-die")
-                .resizable()
-                .scaledToFit()
-                .offset(y: -40)
-            
-            // Contnet
-            VStack {
-                HStack {
-                    Spacer()
-                    Text("James Bond Dies")
-                        .foregroundColor(.red)
-                        .fontWeight(.bold)
-                }
-                .padding(.trailing, 5)
-                .padding(.top, 5)
+        GeometryReader { geo in
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.green)
                 
+                Image("james-bond-no-time-to-die")
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: -40)
                 
-                Spacer()
-                ZStack (alignment: .leading) {
-
-                    Rectangle()
-                        .foregroundColor(.white)
-                        .frame(height: 80)
-                    
-                    VStack (alignment: .leading) {
-                        Text("TheLlast James Bond Movie")
-                            .font(.headline)
-                        Text("The one where James Bond is killled by a missle")
-                            .font(.caption)
+                // Contnet
+                VStack {
+                    HStack {
+                        Spacer()
+                        Text("James Bond Dies")
+                            .foregroundColor(.red)
+                            .fontWeight(.bold)
                     }
-                    .padding(.horizontal)
+                    .padding(.trailing, 5)
+                    .padding(.top, 5)
+                    
+                    
+                    Spacer()
+                    ZStack (alignment: .leading) {
+
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(height: 80)
+                        
+                        VStack (alignment: .leading) {
+                            Text("The Last James Bond Movie")
+                                .font(.headline)
+                            Text("The one where James Bond is killled by a missle")
+                                .font(.caption)
+                        }
+                        .padding(.horizontal)
+                    }
+                    
                 }
-                
             }
-        }
-        .frame(height: 420)
-        .cornerRadius(15)
-        .shadow(radius: 10)
+            .frame(height: 420)
+            .cornerRadius(15)
+            .shadow(radius: 10)
         .padding()
+        }
     }
 }
 
