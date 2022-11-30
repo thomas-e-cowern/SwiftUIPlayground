451ea10d4a908a4de45db07e1c1c75ba2312a269
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-30 02:17:22 -0500

    add text and formatting to column section in favoritesview

--- CodeWithChris/CustomTabView/CustomTabView/Views/FavoritesView.swift
+++ CodeWithChris/CustomTabView/CustomTabView/Views/FavoritesView.swift
@@ -14,13 +14,31 @@ struct FavoritesView: View {
     var flexibleColumns = Array(repeating: GridItem(.fixed(100), spacing: 5, alignment: .center), count: 2)
     
     var body: some View {
-        LazyVGrid(columns: fixedColumns) {
-            ForEach(0..<5) { index in
-                Text("Index: \(index)")
+        VStack {
+            Text("Fixed Columns")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.teal)
+            
+            LazyVGrid(columns: fixedColumns) {
+                ForEach(0..<5) { index in
+                    Text("Index: \(index)")
+                }
             }
+            .padding(.bottom, 20)
+            
+            Text("Flexible Columns")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.yellow)
+            
+            LazyVGrid(columns: fixedColumns) {
+                ForEach(0..<5) { index in
+                    Text("Index: \(index)")
+                }
+            }
+            .padding(.bottom, 20)
         }
-        
-        
     }
 }
 
