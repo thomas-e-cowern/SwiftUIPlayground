cb82e1b36d7f3e55c4cce113e0fe443708f8dc0c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-05 04:42:01 -0500

    add State variables and data to ContentView

--- PieChart/PieChart/ContentView.swift
+++ PieChart/PieChart/ContentView.swift
@@ -74,6 +74,21 @@ struct PieChart: View {
 }
 
 struct ContentView: View {
+    
+    @State private var redAmount = Double.random(in: 10...100)
+    @State private var yellowAmount = Double.random(in: 10...100)
+    @State private var greenAmount = Double.random(in: 10...100)
+    @State private var blueAmount = Double.random(in: 10...100)
+    
+    var data: [DataPoint] {
+        [
+            DataPoint(id: 1, value: redAmount, color: .red),
+            DataPoint(id: 2, value: yellowAmount, color: .yellow),
+            DataPoint(id: 3, value: greenAmount, color: .green),
+            DataPoint(id: 4, value: blueAmount, color: .blue)
+        ]
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
