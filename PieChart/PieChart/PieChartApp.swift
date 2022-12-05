750eedaf203cde1c67dfab4e8dbe1184752bcbef
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-05 04:16:59 -0500

    PieChart initial comit

---
+++ PieChart/PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Thomas Cowern on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
