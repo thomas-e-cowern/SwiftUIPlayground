23fc0ed072c186f7898456e51d4f806e2938007d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-05 04:23:37 -0500

    add init to DataPoint struct

--- PieChart/PieChart/ContentView.swift
+++ PieChart/PieChart/ContentView.swift
@@ -11,6 +11,18 @@ struct DataPoint: Identifiable {
     var id: Int
     var value: Double
     var color: Color
+    
+    init(value: Double, color: Color) {
+        self.id = Int.random(in: 1..<Int.max)
+        self.value = value
+        self.color = color
+    }
+    
+    init(id: Int, value: Double, color: Color) {
+        self.id = id
+        self.value = value
+        self.color = color
+    }
 }
 
 struct ContentView: View {
