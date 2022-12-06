904cab31f7f446f2bde77eed3cd66672df2900ee
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-06 03:55:28 -0500

    BarChart initial commit

---
+++ BarChart/BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Thomas Cowern on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
