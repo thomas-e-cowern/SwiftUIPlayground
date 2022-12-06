8819af6e5c20f3e52216d330cf376f8516f8ec99
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-06 08:19:48 -0500

    add DataPoints to contentView

--- BarChart/BarChart/Views/ContentView.swift
+++ BarChart/BarChart/Views/ContentView.swift
@@ -14,6 +14,15 @@ struct ContentView: View {
     @State private var greenAmount = Double.random(in: 10...100)
     @State private var blueAmount = Double.random(in: 10...100)
     
+    var data: [DataPoint] {
+        [
+            DataPoint(id: 1, value: redAmount, color: .red, title: "Yes"),
+            DataPoint(id: 2, value: yellowAmount, color: .yellow, title: "Maybe"),
+            DataPoint(id: 3, value: greenAmount, color: .green, title: "No"),
+            DataPoint(id: 4, value: blueAmount, color: .blue, title: "N/A")
+        ]
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
