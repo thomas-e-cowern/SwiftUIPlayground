d731ecfadaef4ad16816c780d9f12b7b427845cb
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-06 04:14:34 -0500

    update BarChart with init and View

--- BarChart/BarChart/Models/BarChart.swift
+++ BarChart/BarChart/Models/BarChart.swift
@@ -5,4 +5,30 @@
 //  Created by Thomas Cowern on 12/6/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct BarChart: View {
+    let dataPoints: [DataPoint]
+    let maxValue: Double
+
+    init(dataPoints: [DataPoint]) {
+        self.dataPoints = dataPoints
+
+        let highestPoint = dataPoints.max { $0.value < $1.value }
+        maxValue = highestPoint?.value ?? 1
+    }
+    
+    var body: some View {
+        HStack {
+            ForEach(dataPoints) { data in
+                Rectangle()
+                    .fill(data.color)
+                    .scaleEffect(y: CGFloat(data.value / maxValue))
+                    .overlay(
+                        Text(data.title)
+                            .bold()
+                    )
+            }
+        }
+    }
+}
