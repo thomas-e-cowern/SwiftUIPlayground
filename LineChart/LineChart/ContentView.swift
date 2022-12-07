3a8116a05eb0e3fdaaf21b7e71dfd57e08368a96
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-07 04:29:53 -0500

    add makeDataPoints method to ContentVuew

--- LineChart/LineChart/ContentView.swift
+++ LineChart/LineChart/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var data = makeDataPoints()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +19,31 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+    
+    static func makeDataPoints() -> [DataPoint] {
+        var isGoingUp = true
+        var currentValue = 50.0
+
+        return (1...50).map { _ in
+            if isGoingUp {
+                currentValue += Double.random(in: 1...10)
+            } else {
+                currentValue += -Double.random(in: 1...10)
+            }
+
+            if isGoingUp {
+                if Int.random(in: 0..<10) == 0 {
+                    isGoingUp.toggle()
+                }
+            } else {
+                if Int.random(in: 0..<7) == 0 {
+                    isGoingUp.toggle()
+                }
+            }
+
+            return DataPoint(value: abs(currentValue))
+        }
     }
 }
 
