f285058a28c02053de256cf802245f83d504cd01
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-07 10:32:44 -0500

    update LineChart View with point functionality

--- LineChart/LineChart/Views/LineChart.swift
+++ LineChart/LineChart/Views/LineChart.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct LineChart: View {
     let dataPoints: [DataPoint]
-        var lineColor = Color.primary
-        var lineWidth: CGFloat = 2
+    var lineColor = Color.primary
+    var lineWidth: CGFloat = 2
+    var pointSize: CGFloat = 5
+    var pointColor = Color.primary
 
-        var pointSize: CGFloat = 5
+    var body: some View {
+        ZStack {
+            if lineColor != .clear {
+                LineChartShape(dataPoints: dataPoints, pointSize: pointSize, drawingLines: true)
+                    .stroke(lineColor, lineWidth: lineWidth)
+            }
 
-        var body: some View {
-            ZStack {
-                if lineColor != .clear {
-                    LineChartShape(dataPoints: dataPoints, pointSize: pointSize)
-                        .stroke(lineColor, lineWidth: lineWidth)
-                }
+            if pointColor != .clear {
+                LineChartShape(dataPoints: dataPoints, pointSize: pointSize, drawingLines: false)
+                    .fill(pointColor)
             }
         }
+    }
 }
 
 struct LineChart_Previews: PreviewProvider {
