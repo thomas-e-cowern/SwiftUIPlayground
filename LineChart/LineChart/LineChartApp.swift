7fad4358ce473516c926d80fa972514fd8d487ea
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-07 04:07:55 -0500

    LineChart initial commit

---
+++ LineChart/LineChart/LineChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineChartApp.swift
+//  LineChart
+//
+//  Created by Thomas Cowern on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LineChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
