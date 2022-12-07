1de78cd56d31c4e27ff88afa34c40b4c56c926f1
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-07 04:33:00 -0500

    add LineChartShape to Models folder

---
+++ LineChart/LineChart/Models/LineChartShape.swift
@@ -0,0 +1,18 @@
+//
+//  LineChartShape.swift
+//  LineChart
+//
+//  Created by Thomas Cowern on 12/7/22.
+//
+
+import SwiftUI
+
+struct LineChartShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        <#code#>
+    }
+    
+    let dataPoints: [DataPoint]
+    let pointSize: CGFloat
+    let maxValue: Double
+}
