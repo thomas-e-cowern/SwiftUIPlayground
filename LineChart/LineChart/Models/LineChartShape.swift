df3b5372b845f7e0c48b43f672a3bdb1f7f4ba97
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-07 04:57:36 -0500

    add init and path method to LineChartShape

--- LineChart/LineChart/Models/LineChartShape.swift
+++ LineChart/LineChart/Models/LineChartShape.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct LineChartShape: Shape {
-    func path(in rect: CGRect) -> Path {
-        <#code#>
-    }
     
     let dataPoints: [DataPoint]
     let pointSize: CGFloat
     let maxValue: Double
+    
+    init(dataPoints: [DataPoint], pointSize: CGFloat) {
+        self.dataPoints = dataPoints
+        self.pointSize = pointSize
+
+        let highestPoint = dataPoints.max { $0.value < $1.value }
+        maxValue = highestPoint?.value ?? 1
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+            let drawRect = rect.insetBy(dx: pointSize, dy: pointSize)
+
+            // more code to come
+    }
+    
 }
