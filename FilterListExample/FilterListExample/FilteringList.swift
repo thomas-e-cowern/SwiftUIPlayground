86ab8686e61396b681dd919fc7b4267d50bc064f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-08 05:44:27 -0500

    add FilteringList to project

---
+++ FilterListExample/FilterListExample/FilteringList.swift
@@ -0,0 +1,20 @@
+//
+//  FilteringList.swift
+//  FilterListExample
+//
+//  Created by Thomas Cowern on 12/8/22.
+//
+
+import SwiftUI
+
+struct FilteringList<T: Identifiable, Content: View>: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct FilteringList_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FilteringList()
+//    }
+//}
