6754a31f38addefbe14ebafb76a88292d7bad5fc
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-08 05:46:42 -0500

    move VStack and applyFilter method to FilteringList

--- FilterListExample/FilterListExample/FilteringList.swift
+++ FilterListExample/FilterListExample/FilteringList.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct FilteringList<T: Identifiable, Content: View>: View {
+    
+    @State private var filteredItems = [T]()
+    @State private var filterString = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Type to filter", text: $filterString.onChange(applyFilter))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+
+            List(filteredItems) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                        .font(.headline)
+                    Text(user.address)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .onAppear(perform: applyFilter)
+
+        }
+    }
+    
+    func applyFilter() {
+        let cleanedFilter = filterString.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        if cleanedFilter.isEmpty {
+            filteredItems = users
+        } else {
+            filteredItems = users.filter { $0.name
+                    .localizedCaseInsensitiveContains(cleanedFilter) }
+        }
     }
 }
 
--- FilterListExample/FilterListExample/Views/ContentView.swift
+++ FilterListExample/FilterListExample/Views/ContentView.swift
@@ -10,44 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     let users = Bundle.main.decode([User].self, from: "users.json")
-    @State private var filteredItems = [User]()
-    @State private var filterString = ""
     
     var body: some View {
         VStack {
             NavigationView {
-                VStack {
-                    TextField("Type to filter", text: $filterString.onChange(applyFilter))
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.horizontal)
-
-                    List(filteredItems) { user in
-                        VStack(alignment: .leading) {
-                            Text(user.name)
-                                .font(.headline)
-                            Text(user.address)
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    .onAppear(perform: applyFilter)
-
-                }
+                
                 .navigationBarTitle("Address Book")
             }
         }
         .padding()
     }
     
-    func applyFilter() {
-        let cleanedFilter = filterString.trimmingCharacters(in: .whitespacesAndNewlines)
 
-        if cleanedFilter.isEmpty {
-            filteredItems = users
-        } else {
-            filteredItems = users.filter { $0.name
-                    .localizedCaseInsensitiveContains(cleanedFilter) }
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
