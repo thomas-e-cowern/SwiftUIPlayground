7848aa6667725432727031dc9d7e4fa143fb7156
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-08 05:38:43 -0500

    add Bindig+Extensions to Extensions folder

---
+++ FilterListExample/FilterListExample/Extensions/Binding+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Binding+Extensions.swift
+//  FilterListExample
+//
+//  Created by Thomas Cowern on 12/8/22.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
