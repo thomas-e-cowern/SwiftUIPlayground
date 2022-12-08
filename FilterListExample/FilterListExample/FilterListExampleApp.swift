a5f906fe5e2c1cd60539b210974bc5bc4235f4db
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-08 05:24:42 -0500

    FilterListExample initial commit

---
+++ FilterListExample/FilterListExample/FilterListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterListExampleApp.swift
+//  FilterListExample
+//
+//  Created by Thomas Cowern on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FilterListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
