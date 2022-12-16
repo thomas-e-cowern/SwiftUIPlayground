e671b6459c240ccea80b7443dc4f65d97849000e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 12:50:47 -0500

    update Constants with PLAYLISTID

--- YouTubeViewer/YouTubeViewer/Helpers/Constants.swift
+++ YouTubeViewer/YouTubeViewer/Helpers/Constants.swift
@@ -10,4 +10,5 @@ import Foundation
 struct Constants {
     static let API_KEY = "AIzaSyD3Lstg8qSb2uw5PILjfpoBUwpiWss2hYY"
     static let API_URL = "https://www.googleapis.com/youtube/v3"
+    static let PLAYLISTID = "PLfSGz2lo5ka0zKazCfw1hm9-RLa2RrbI7"
 }
