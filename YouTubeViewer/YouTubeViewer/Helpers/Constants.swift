e585f7301794c0acc229b2e95cd23f0008890ac1
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 09:39:18 -0500

    add Helpers folder and Constants file, add API_KEY to Constants file

---
+++ YouTubeViewer/YouTubeViewer/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  YouTubeViewer
+//
+//  Created by Thomas Cowern on 12/16/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "AIzaSyD3Lstg8qSb2uw5PILjfpoBUwpiWss2hYY"
+}
