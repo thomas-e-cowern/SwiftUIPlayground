f66c8df3b9d9bc46623c2e0e458ad14d942e86a0
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 11:41:13 -0500

    update Response model with enum and init

--- YouTubeViewer/YouTubeViewer/Models/Response.swift
+++ YouTubeViewer/YouTubeViewer/Models/Response.swift
@@ -11,4 +11,14 @@ struct Response: Decodable {
     
     var items: [Video]
     
+    enum CodingKeys: String, CodingKey {
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+    
 }
