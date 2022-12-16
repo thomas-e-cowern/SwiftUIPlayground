8497a97b05699c40da3d2d55f7ba6821765bfaec
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 12:48:45 -0500

    update VideoModel wtih init and getVIdeos method

--- YouTubeViewer/YouTubeViewer/ViewModels/VideoModel.swift
+++ YouTubeViewer/YouTubeViewer/ViewModels/VideoModel.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import Alamofire
 
 class VideoModel: ObservableObject {
+    @Published var videos = [Video]()
+    
+    init() {
+        getVideos()
+    }
+    
+    func getVideos() {
+        // crate url object
+        guard let url = URL(string: "\(Constants.API_URL)/playListItems") else {
+            print("DEBUG: Problem getting url in VideoModel")
+            return
+        }
+        
+        // Decoder
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        // Crate URL request
+        AF.request(
+            url,
+            parameters: ["part":"snippet" "playlistId":]
+        )
+    }
     
 }
