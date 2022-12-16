7670054e676813e2eedfef31eb3805e564069825
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 11:36:17 -0500

    add Models folder and Response file to project

---
+++ YouTubeViewer/YouTubeViewer/Models/Response.swift
@@ -0,0 +1,14 @@
+//
+//  Response.swift
+//  YouTubeViewer
+//
+//  Created by Thomas Cowern on 12/16/22.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Videos]
+    
+}
