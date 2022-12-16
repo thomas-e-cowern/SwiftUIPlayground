7a18a272cd61300fe1b4340f5124a168c1c45788
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-16 08:48:09 -0500

    YouTubeViewer inital commit

---
+++ YouTubeViewer/YouTubeViewer/YouTubeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeViewerApp.swift
+//  YouTubeViewer
+//
+//  Created by Thomas Cowern on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
