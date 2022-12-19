7d7af17b8300d22fc7e1f6dbf6762723e4828c8a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-19 14:57:04 -0500

    update YoutubeVideoPlayer makeUIView

--- YouTubeViewer/YouTubeViewer/Views/YoutubeVideoPlayer.swift
+++ YouTubeViewer/YouTubeViewer/Views/YoutubeVideoPlayer.swift
@@ -12,11 +12,23 @@ struct YoutubeVideoPlayer: UIViewRepresentable {
     var video: Video
     
     func makeUIView(context: Context) -> some UIView {
-        <#code#>
+        // create webview
+        let view = WKWebView()
+        
+        // create url
+        let embedUrlString = Constants.YT_EMBED_URL + video.videoId
+        
+        // load the video in webview
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        view.load(request)
+        
+        // return webview
+        return view
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        <#code#>
+        
     }
 }
 
