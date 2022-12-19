5f08c8bf121f8a29fc9ae4eba18734e24751ecd4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-19 15:43:40 -0500

    update Home view

--- YouTubeViewer/YouTubeViewer/Views/Home.swift
+++ YouTubeViewer/YouTubeViewer/Views/Home.swift
@@ -7,9 +7,24 @@
 
 import SwiftUI
 
+let backgroundColor = Color(red: 31 / 255, green: 33 / 255, blue: 36 / 255)
+
 struct Home: View {
+    @StateObject var model = VideoModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVStack {
+                ForEach(model.videos, id:\.videoId) { video in
+                    VideoRow(videoPreview: VideoPreview(video: video))
+                        .padding()
+                }
+            }
+            .padding(.top, 20)
+        }
+        .foregroundColor(.white)
+        .background(backgroundColor)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
