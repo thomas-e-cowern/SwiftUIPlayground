17b759403b728364572707a979ac0c86e7dbd806
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-19 15:21:21 -0500

    add VideoRow

---
+++ YouTubeViewer/YouTubeViewer/Views/VideoRow.swift
@@ -0,0 +1,20 @@
+//
+//  VideoRow.swift
+//  YouTubeViewer
+//
+//  Created by Thomas Cowern on 12/19/22.
+//
+
+import SwiftUI
+
+struct VideoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoRow()
+    }
+}
