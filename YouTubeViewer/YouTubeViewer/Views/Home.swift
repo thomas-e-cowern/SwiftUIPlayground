ec1be8c9fbb553454b5c1fca33db3f3964d7d59c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-19 15:37:39 -0500

    add Home view

---
+++ YouTubeViewer/YouTubeViewer/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  YouTubeViewer
+//
+//  Created by Thomas Cowern on 12/19/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
