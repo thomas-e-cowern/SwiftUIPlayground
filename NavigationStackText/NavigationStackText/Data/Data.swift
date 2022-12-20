d9f1f9cc9b10651b64bcbcae825b1b0c626c9089
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-20 08:51:45 -0500

    add games variable to Data

--- NavigationStackText/NavigationStackText/Data/Data.swift
+++ NavigationStackText/NavigationStackText/Data/Data.swift
@@ -12,4 +12,12 @@ struct Data {
                                  .init(name: "Playstation", imageName: "playstation-logo", color: .indigo),
                                  .init(name: "PC", imageName: "pc-logo", color: .gray),
                                  .init(name: "IPhone", imageName: "iphone-logo", color: .black)]
+    
+    var games: [Game] = [.init(name: "Minecraft", rating: "92"),
+                         .init(name: "God of war", rating: "89"),
+                         .init(name: "Fortnite", rating: "99"),
+                         .init(name: "Madden 2023", rating: "93")]
+
 }
+
+
