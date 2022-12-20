7fb3dd92495fe219c631dd47d6c5cbcaa291d5a4
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-20 08:46:13 -0500

    add Game mdoel to project

---
+++ NavigationStackText/NavigationStackText/Models/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  NavigationStackText
+//
+//  Created by Thomas Cowern on 12/20/22.
+//
+
+import SwiftUI
+
+struct Game: Hashable {
+    var name: String
+    var rating: String
+}
