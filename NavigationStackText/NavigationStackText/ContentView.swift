ee844a499f599cc60b74236287623d0891553bc6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-20 07:57:38 -0500

    add NavigationStack and navigationTitle to ContentView

--- NavigationStackText/NavigationStackText/ContentView.swift
+++ NavigationStackText/NavigationStackText/ContentView.swift
@@ -9,25 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            List {
-                Section("Platforms") {
-                    ForEach(PlatformData().platforms, id:\.name) { platform in
-                        HStack {
-                            Image(platform.imageName)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50)
-                                .padding(.trailing, 5)
-                            Text(platform.name)
-                                .foregroundColor(platform.color)
-                                .font(.title2)
+        NavigationStack {
+            VStack {
+                List {
+                    Section("Platforms") {
+                        ForEach(PlatformData().platforms, id:\.name) { platform in
+                            HStack {
+                                Image(platform.imageName)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50)
+                                    .padding(.trailing, 5)
+                                Text(platform.name)
+                                    .foregroundColor(platform.color)
+                                    .font(.title2)
+                            }
                         }
                     }
                 }
+                .navigationTitle("Gaming")
             }
+            .padding()
         }
-        .padding()
     }
 }
 
