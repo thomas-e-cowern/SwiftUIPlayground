a1ec3cf15ada671301dad3c905a9043fab51961d
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-20 07:12:39 -0500

    add platforms variable to PlatformData

---
+++ NavigationStackText/NavigationStackText/Data/PlatformData.swift
@@ -0,0 +1,15 @@
+//
+//  PlatformData.swift
+//  NavigationStackText
+//
+//  Created by Thomas Cowern on 12/20/22.
+//
+
+import Foundation
+
+struct PlatformData {
+    var platforms: [Platform] = [.init(name: "XBox", imageName: <#T##String#>, color: .green),
+                                 .init(name: "Playstation", imageName: <#T##String#>, color: .indigo),
+                                 .init(name: "PC", imageName: <#T##String#>, color: .gray),
+                                 .init(name: "IPhone", imageName: <#T##String#>, color: .black)]
+}
