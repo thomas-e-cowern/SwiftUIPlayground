f67c0ef23cda9e44a87f677b5e53f41be44ed065
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-22 08:35:06 -0500

    add Views folder and HomeView

---
+++ SwiftUIReminders/SwiftUIReminders/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SwiftUIReminders
+//
+//  Created by Thomas Cowern on 12/22/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
