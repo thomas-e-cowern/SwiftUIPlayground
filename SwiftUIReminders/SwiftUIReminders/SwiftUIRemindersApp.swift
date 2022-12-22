2d2c40b503f5af5120c52c0b3c4b37e38963ab59
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-22 08:32:31 -0500

    SwiftUIReminders initial commit

---
+++ SwiftUIReminders/SwiftUIReminders/SwiftUIRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRemindersApp.swift
+//  SwiftUIReminders
+//
+//  Created by Thomas Cowern on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
